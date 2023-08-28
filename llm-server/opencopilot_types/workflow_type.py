56b9184c5a55ba058b29d2d43e062ce3b6f6d767
codebanesr <shanurrahman@shanurs-MacBook-Pro.local>
2023-08-29 02:53:24 +0300

    Adding simple functions for creating and updating workflows

---
+++ llm-server/opencopilot_types/workflow_type.py
@@ -0,0 +1,20 @@
+from typing import TypedDict, List
+
+class WorkflowStepType(TypedDict):
+    stepId: str
+    operation: str
+    open_api_operation_id: str
+    parameters: dict
+
+class WorkflowFlowType(TypedDict):
+    name: str
+    description: str
+    requires_confirmation: bool
+    steps: List[WorkflowStepType]
+    on_success: List[dict]
+    on_failure: List[dict]
+
+class WorkflowDataType(TypedDict):
+    opencopilot: str
+    info: dict
+    flows: List[WorkflowFlowType]
