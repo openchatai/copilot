b940fb1cc1bb0fc27ac320d0d5ccd22fe7cdc34c
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-01 00:08:34 +0300

    fixed:BASE_URL bug

--- dashboard/utils/endpoints.ts
+++ dashboard/utils/endpoints.ts
@@ -1,4 +1,4 @@
 
 const isDEV = process.env.NODE_ENV === 'development'
-export const BASE_BACKEND_URL = isDEV ? "http://localhost:5000" : "http://backend:8000"
+export const BASE_BACKEND_URL = isDEV ? "http://localhost:5000" : "http://backend:5000"
 export const BASE_DASHBOARD_URL = "http://127.0.0.1:8000/api" // @todo
