8abb111fc88a08051c645b1fc1193374932610d2
gharbat <arbioun@gmail.com>
2023-09-02 16:51:02 +0300

    Use Nginx proxy to avoid CORS issues

--- dashboard/utils/endpoints.ts
+++ dashboard/utils/endpoints.ts
@@ -1,6 +1,6 @@
 import { IS_SERVER } from "./is_server"
 
 const isDEV = process.env.NODE_ENV === 'development'
-export const BASE_BACKEND_URL = !IS_SERVER ? "http://localhost:5000" : "http://backend:5000"
+export const BASE_BACKEND_URL = !IS_SERVER ? "http://localhost:8888/backend" : "http://backend:5000"
 // export const BASE_BACKEND_URL = "http://0.0.0.0:5000"
 export const BASE_DASHBOARD_URL = "http://127.0.0.1:8000/api" // @todo
