0b442651ec81c5b18d7c1819d6b1a01f387abdf1
codebanesr <shanurrahman@shanurs-MacBook-Pro.local>
2023-09-05 14:11:00 +0300

    removing all pyc files

--- llm-server/api_caller/base.py
+++ llm-server/api_caller/base.py
@@ -1,7 +1,7 @@
 from langchain.chains.openai_functions.openapi import get_openapi_chain
+from langchain.utilities.openapi import OpenAPISpec
 
-
-def try_to_match_and_call_api_endpoint(swagger_spec, text, headers):
+def try_to_match_and_call_api_endpoint(swagger_spec: OpenAPISpec, text, headers):
     openapi_call_chain = get_openapi_chain(swagger_spec, verbose=True, headers=headers)
 
     return openapi_call_chain.run(text)
