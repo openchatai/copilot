28b148732fe0c998c2f19a908dcda379e515ea17
codebanesr <shanur.cse.nitap@gmail.com>
2023-09-11 07:13:37 +0300

    Adding simple functions for creating and updating workflows

--- llm-server/routes/workflow/workflow.py
+++ llm-server/routes/workflow/workflow.py
@@ -3,6 +3,10 @@ from app import mongo
 from validate_json import validate_json
 from bson import ObjectId
 from copilot_exceptions.handle_exceptions_and_errors import handle_exceptions_and_errors 
+from utils.vector_db.qdrant import QdrantVectorDBClient
+from utils.get_embeddings import get_embeddings
+from opencopilot_types.workflow_type import WorkflowDataType
+import warnings
 bp = Blueprint('workflow', __name__)
 import json
 
@@ -18,17 +22,45 @@ def get_workflow(workflow_id):
 @validate_json(workflow_schema)
 @handle_exceptions_and_errors
 def create_workflow():
-    workflow_data = request.json
+    workflow_data: WorkflowDataType = request.json
     workflows = mongo.db.workflows
-    inserted_id = workflows.insert_one(workflow_data).inserted_id
-    return jsonify({'message': 'Workflow created', 'inserted_id': str(inserted_id)}), 201
+    workflow_id = workflows.insert_one(workflow_data).workflow_id
+
+    qdrant_client = QdrantVectorDBClient()
+    namespace = "workflows"
+    # Check if the namespace is generic
+    if namespace == "workflows":
+        warning_message = "Warning: The 'namespace' variable is set to the generic value 'workflows'. You should replace it with a specific value for your org / user / account."
+        warnings.warn(warning_message, UserWarning)
+    
+    embedding_provider = get_embeddings()
+    for flow in workflow_data["flows"]:
+        vectors = embedding_provider.embed_query(flow["description"])
+        meta = {"workflow_id": str(workflow_id), "workflow_name": workflow_data.get("name")}
+        qdrant_client.add_data_with_meta(namespace, vectors, meta)
+
+    return jsonify({'message': 'Workflow created', 'workflow_id': str(workflow_id)}), 201
 
 @bp.route('/workflow/<workflow_id>', methods=['PUT'])
 @validate_json(workflow_schema)
 @handle_exceptions_and_errors
 def update_workflow(workflow_id):
-    updated_workflow = request.json
+    updated_workflow: WorkflowDataType = request.json
     mongo.db.workflows.update_one({'_id': ObjectId(workflow_id)}, {'$set': updated_workflow})
+    qdrant_client = QdrantVectorDBClient()
+    qdrant_client.delete_documents_by_workflow_id(workflow_id)
+    namespace = "workflows"
+    # Check if the namespace is generic
+    if namespace == "workflows":
+        warning_message = "Warning: The 'namespace' variable is set to the generic value 'workflows'. You should replace it with a specific value for your org / user / account."
+        warnings.warn(warning_message, UserWarning)
+    
+    embedding_provider = get_embeddings()
+    for flow in updated_workflow["flows"]:
+        vectors = embedding_provider.embed_query(flow["description"])
+        meta = {"workflow_id": str(workflow_id), "workflow_name": updated_workflow.get("name")}
+        qdrant_client.add_data_with_meta(namespace, vectors, meta)
+
     return jsonify({'message': 'Workflow updated'}), 200
 
 @bp.route('/workflow/<workflow_id>', methods=['DELETE'])
