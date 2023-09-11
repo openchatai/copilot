0289a3e9a7f655499f8b425d718b21601e976c0b
codebanesr <shanur.cse.nitap@gmail.com>
2023-09-11 07:15:36 +0300

    Adding readme for starting the application locally Adding docker containers for llm server

--- llm-server/app.py
+++ llm-server/app.py
@@ -1,15 +1,17 @@
 import warnings
+
+import requests
 from flask import Flask, request
 from langchain.chains.openai_functions import create_structured_output_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 
+from langchain.utilities.openapi import OpenAPISpec
 from utils.base import try_to_match_and_call_api_endpoint
 from models.models import AiResponseFormat
 from flask_pymongo import PyMongo
 import os
 from routes.workflow.workflow_controller import workflow
-from utils.fetch_swagger_spec import fetch_swagger_spec
 import json
 import logging
 from typing import Any, Tuple
@@ -23,11 +25,10 @@ mongo = PyMongo(app)
 
 app.register_blueprint(workflow, url_prefix="/workflow")
 
-# TODO: Implement caching for the swagger file content (no need to load it everytime)
 
-
+## TODO: Implement caching for the swagger file content (no need to load it everytime)
 @app.route("/handle", methods=["POST", "OPTIONS"])
-def handle() -> Any:
+def handle():
     data = request.get_json()
     text = data.get("text")
     swagger_url = data.get("swagger_url")
@@ -37,10 +38,28 @@ def handle() -> Any:
     if not text:
         return json.dumps({"error": "text is required"}), 400
 
+    if not swagger_url:
+        return json.dumps({"error": "swagger_url is required"}), 400
+
     if not base_prompt:
         return json.dumps({"error": "base_prompt is required"}), 400
 
-    swagger_spec = fetch_swagger_spec(swagger_url)
+    if swagger_url.startswith("https://"):
+        full_url = swagger_url
+        response = requests.get(full_url)
+        if response.status_code == 200:
+            swagger_text = response.text
+        else:
+            return json.dumps({"error": "Failed to fetch Swagger content"}), 500
+    else:
+        full_url = "/app/shared_data/" + swagger_url
+        try:
+            with open(full_url, "r") as file:
+                swagger_text = file.read()
+        except FileNotFoundError:
+            return json.dumps({"error": "File not found"}), 404
+
+    swagger_spec = OpenAPISpec.from_text(swagger_text)
 
     try:
         json_output = try_to_match_and_call_api_endpoint(swagger_spec, text, headers)
@@ -60,7 +79,7 @@ def handle() -> Any:
     chain = create_structured_output_chain(AiResponseFormat, llm, prompt, verbose=False)
     chain_output = chain.run(question=text)
 
-    return chain_output, 200
+    return json.loads(json.dumps(chain_output.dict())), 200
 
 
 @app.errorhandler(500)
@@ -68,6 +87,7 @@ def internal_server_error(error: Any) -> Tuple[str, int]:
     # Log the error to the console
     print(error)
     return "Internal Server Error", 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8002)
