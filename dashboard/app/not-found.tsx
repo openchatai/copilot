cd16a5e425b5889b0ff4884ab6d2f0293d482175
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-23 14:47:26 +0300

    fixed:chunk load error

--- dashboard/app/not-found.tsx
+++ dashboard/app/not-found.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Button } from "@/ui/components/Button";
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function NotFound() {
