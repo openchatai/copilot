71f9ca577dbdee4b3616aaf3b5c62e173dc3de7c
codebanesr <shanur.cse.nitap@gmail.com>
2023-09-27 14:52:24 +0300

    Adding routes for swagger endpoints, http://localhost:8888/backend/swagger_api/651412babf6060f06810045c

--- llm-server/routes/swagger_controller/swagger_api.py
+++ llm-server/routes/swagger_controller/swagger_api.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify, Blueprint, request, Response
 from flask_pymongo import PyMongo
 
-from bson import ObjectId, json_util
+from bson import ObjectId
 
-app = Flask(__name__)
 from utils.db import Database
 from typing import Any
 
@@ -14,8 +13,27 @@ swagger_workflow = Blueprint("swagger_workflow", __name__)
 
 @swagger_workflow.route("/", methods=["GET"])
 def get_swagger_files() -> Response:
-    files = mongo.db.swagger_files.find_one({}, {"_id": 0})
-    return jsonify(files)
+    # Get page and page_size query params
+    page = int(request.args.get("page", 1))
+    page_size = int(request.args.get("page_size", 10))
+
+    # Calculate document skip/limit
+    skip = (page - 1) * page_size
+    limit = page_size
+
+    # Query for paginated docs
+    files = [
+        doc.update({"_id": str(doc["_id"])}) or doc
+        for doc in mongo.db.swagger_files.find({}, {}).skip(skip).limit(limit)
+    ]
+
+    # Get total docs count
+    total = mongo.db.swagger_files.count_documents({})
+
+    # Prepare response data
+    data = {"total": total, "page": page, "page_size": page_size, "files": files}
+
+    return jsonify(data)
 
 
 @swagger_workflow.route("/", methods=["POST"])
@@ -30,6 +48,8 @@ def get_swagger_file(id: str) -> Response:
     file = mongo.db.swagger_files.find_one({"_id": ObjectId(id)})
     if not file:
         return jsonify({"message": "Swagger file not found"})
+
+    file["_id"] = str(file["_id"])
     return jsonify(file)
 
 
