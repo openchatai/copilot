2fead5302cb5c249e6f0c925c3a68b3fd4fbd441
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-29 22:03:10 +0300

    the flow editor waits for the data.

--- dashboard/app/(dashboard)/app/bot/(bot)/[bot_id]/flows/page.tsx
+++ dashboard/app/(dashboard)/app/bot/(bot)/[bot_id]/flows/page.tsx
@@ -6,9 +6,37 @@ import {
   FlowArena,
 } from "@openchatai/copilot-flows-editor";
 import "@openchatai/copilot-flows-editor/dist/style.css";
-type Props = {};
+import { Button } from "@/ui/components/Button";
+import { useBotData } from "@/ui/providers/BotDataProvider";
+
+const flows = [
+  {
+    id: "1",
+    name: "Add a pet",
+    description:
+      "Add a pet called luna and place order for her 3 days from now",
+    steps: [],
+    createdAt: Date.now(),
+  },
+];
+
 // data-container="bot-layout"
-export default function FlowsPage({}: Props) {
+// load the flows (set the initial state)
+function Header() {
+  const { bot } = useBotData();
+  return (
+    <div className="px-4 py-2 bg-white flex items-center justify-between w-full">
+      <div className="flex items-center">
+        <h1 className="text-lg font-bold">{bot?.name}</h1>
+        <span className="mx-0.5 text-xl">/</span>
+        <h2 className="text-sm text-gray-500 font-semibold"></h2>
+      </div>
+      <Button variant={{ intent: "primary", size: "sm" }}>Save</Button>
+    </div>
+  );
+}
+
+export default function FlowsPage() {
   return (
     <>
       <style jsx global>{`
@@ -16,19 +44,41 @@ export default function FlowsPage({}: Props) {
           padding: 0 !important;
         }
       `}</style>
-      <div className="h-full block absolute inset-0 overflow-hidden">
-        <div className="w-full h-full flex items-start justify-between relative">
-          <Controller
-            initilState={{
-              paths: [],
-              flows: [],
-            }}
-          >
+      <Controller
+        onChange={(data) => console.log(data)}
+        initialState={{
+          paths: [
+            {
+              path: "/flow-1",
+              methods: [
+                {
+                  method: "get",
+                  description: "This is a get method",
+                  operationId: "get-flow-1",
+                  summary: "Get flow 1",
+                  tags: ["flow-1"],
+                },
+                {
+                  method: "post",
+                  description: "This is a get method",
+                  operationId: "get-flow-1",
+                  summary: "Get flow 1",
+                  tags: ["flow-1"],
+                },
+              ],
+            },
+          ],
+          flows: flows,
+        }}
+      >
+        <div className="h-full absolute inset-0 overflow-hidden flex flex-col items-start">
+          <Header />
+          <div className="w-full h-full flex items-start justify-between relative flex-1">
             <FlowArena />
             <CodePreview />
-          </Controller>
+          </div>
         </div>
-      </div>
+      </Controller>
     </>
   );
 }
