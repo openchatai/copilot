9e07e54ff55b280900db97865d2634e7c41d0856
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-10-18 19:53:13 +0300

    added swr, swr-config for data fetching, and copilot context

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -7,6 +7,7 @@ import { IS_DEV } from "@/lib/consts";
 import { SearchModal } from "./(main)/_parts/SearchModal";
 import React from "react";
 import { JotaiProvider } from "./_store/JotaiProvider";
+import { SWRProvider } from "./swr-provider";
 const opensans = Open_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
@@ -23,33 +24,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <JotaiProvider>
-      <html lang="en">
-        <body
-          className={cn(
-            opensans.className,
-            "h-screen min-h-screen w-screen overflow-hidden scroll-smooth bg-background text-accent-foreground antialiased",
-            IS_DEV && "debug-screens",
-          )}
-        >
-          {children}
-          {/* browser too small message */}
-          <div className="fixed inset-0 z-[500] flex items-center justify-center bg-primary-foreground/70 backdrop-blur md:!hidden p-5">
-            <div className="flex flex-col items-center justify-center text-lg">
-              <span>
-                <Tv2 className="h-20 w-20 text-primary" />
-              </span>
-              <h2 className="font-semibold">Your browser is too small</h2>
-              <p className="text-base font-medium">
-                Resize your browser to at least 900px wide to continue.
-              </p>
+    <SWRProvider>
+      <JotaiProvider>
+        <html lang="en">
+          <body
+            className={cn(
+              opensans.className,
+              "h-screen min-h-screen w-screen overflow-hidden scroll-smooth bg-background text-accent-foreground antialiased",
+              IS_DEV && "debug-screens",
+            )}
+          >
+            {children}
+            {/* browser too small message */}
+            <div className="fixed inset-0 z-[500] flex items-center justify-center bg-primary-foreground/70 p-5 backdrop-blur md:!hidden">
+              <div className="flex flex-col items-center justify-center text-lg">
+                <span>
+                  <Tv2 className="h-20 w-20 text-primary" />
+                </span>
+                <h2 className="font-semibold">Your browser is too small</h2>
+                <p className="text-base font-medium">
+                  Resize your browser to at least 900px wide to continue.
+                </p>
+              </div>
             </div>
-          </div>
 
-          {/* search modal */}
-          <SearchModal />
-        </body>
-      </html>
-    </JotaiProvider>
+            {/* search modal */}
+            <SearchModal />
+          </body>
+        </html>
+      </JotaiProvider>
+    </SWRProvider>
   );
 }
