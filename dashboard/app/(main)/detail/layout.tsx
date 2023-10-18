b04e540c251707f9dd9de42c76396680421f3e9f
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-10-18 21:06:32 +0300

    deleted detail route

--- dashboard/app/(main)/detail/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function DetailRouteLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
--- dashboard/components/domain/CopilotCardSmall.tsx
+++ dashboard/components/domain/CopilotCardSmall.tsx
@@ -1,11 +1,4 @@
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-} from "@/components/ui/dropdown-menu";
 import { BotIcon, Terminal } from "lucide-react";
-import { Button } from "../ui/button";
 import Link from "next/link";
 import {
   Tooltip,
@@ -17,33 +10,11 @@ import {
 export function CopilotCardSmall() {
   return (
     <div className="xl:aspect-square">
-      <div className="rounded-lg relative h-56 border bg-secondary shadow-sm flex items-center justify-center p-5 group">
-        <div className="inset-0 absolute backdrop-blur-sm bg-accent-alt/50 opacity-0 group-hover:opacity-100 group-focus-visible:opacity-100 group-focus-within:opacity-100 transition-opacity">
-          <div className="h-full w-full gap-2 flex items-center justify-center">
-            <Button size="lg" asChild>
-              <Link href={"/copilot/12346879798/settings"}>Edit</Link>
-            </Button>
-
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button size="lg" variant="outline">
-                  ...
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>Rename</DropdownMenuItem>
-                <DropdownMenuItem>Detail</DropdownMenuItem>
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Duplicate</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </div>
-
-        <div className="left-2 top-2 absolute">
+      <div className="group relative flex h-56 items-center justify-center rounded-lg border bg-secondary p-5 shadow-sm">
+        <div className="absolute left-2 top-2">
           <TooltipProvider>
             <Tooltip>
-              <TooltipTrigger className="p-1.5 rounded-full bg-white shadow-lg">
+              <TooltipTrigger className="rounded-full bg-white p-1.5 shadow-lg">
                 <Terminal className="h-4 w-4" />
               </TooltipTrigger>
               <TooltipContent>
@@ -53,15 +24,15 @@ export function CopilotCardSmall() {
           </TooltipProvider>
         </div>
         <div>
-          <div className="h-20 grid place-content-center aspect-square rounded-lg bg-slate-950 text-gray-100">
+          <div className="grid aspect-square h-20 place-content-center rounded-lg bg-slate-950 text-gray-100">
             <BotIcon className="h-12 w-12" />
           </div>
         </div>
       </div>
       <div className="mt-1.5 ps-1">
         <Link
-          href="/detail/copilot/12346879798"
-          className="text-sm font-semibold whitespace-nowrap line-clamp-1 text-ellipsis"
+          href="/copilot/12346879798"
+          className="line-clamp-1 text-ellipsis whitespace-nowrap text-sm font-semibold"
         >
           Copilot 1
         </Link>
