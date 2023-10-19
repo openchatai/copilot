2216a8a8673bfe46320a3aed0e304a158291dea1
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-10-19 18:17:24 +0300

    delay duration 100ms

--- dashboard/app/(copilot)/_parts/CopilotNavLink.tsx
+++ dashboard/app/(copilot)/_parts/CopilotNavLink.tsx
@@ -21,7 +21,7 @@ export function CopilotLayoutNavLink({
   segment?: string;
 }) {
   return (
-    <TooltipProvider>
+    <TooltipProvider delayDuration={100}>
       <Tooltip>
         <TooltipTrigger asChild>
           <NavLink
