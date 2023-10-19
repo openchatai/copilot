740aa7298baad0e4e10f07ca410358656288c9bc
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-10-19 18:12:55 +0300

    created simple `timesince` function

---
+++ dashboard/lib/timesince.ts
@@ -0,0 +1,25 @@
+export function timeSince(date: string | Date) {
+  const $date = new Date(date);
+  const seconds = Math.floor((new Date().getTime() - $date.getTime()) / 1000);
+  let interval = seconds / 31536000;
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
