49b04f7ff76e1eaa0a6887c27203c7a4d063a516
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-10-22 03:24:29 +0300

    removing searchParams when they are empty.

--- dashboard/app/(main)/_parts/SearchModal.tsx
+++ dashboard/app/(main)/_parts/SearchModal.tsx
@@ -18,63 +18,66 @@ import {
   CommandItem,
   CommandList,
   CommandSeparator,
-  CommandShortcut,
 } from "@/components/ui/command";
 import { searchModalAtom } from "@/app/_store/atoms/searchModal";
 import { useAtom } from "jotai";
-
+import { useHotkeys } from "react-hotkeys-hook";
+import { EmptyBlock } from "@/components/domain/EmptyBlock";
+import useSwr from "swr";
+import { listCopilots } from "@/data/copilot";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 export function SearchModal() {
   const [open, setOpen] = useAtom(searchModalAtom);
-
-  React.useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === "t" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault();
-        setOpen((open) => !open);
-      }
-    };
-
-    document.addEventListener("keydown", down);
-    return () => document.removeEventListener("keydown", down);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+  useHotkeys("ctrl+/", (e) => {
+    e.preventDefault();
+    setOpen(true);
+  });
+  const { data: copilots } = useSwr("copilotsList", listCopilots);
+  const { push } = useRouter();
   return (
     <CommandDialog open={open} onOpenChange={setOpen}>
       <CommandInput placeholder="Type a command or search..." />
       <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandEmpty>
+          <EmptyBlock>
+            <p>No results found.</p>
+          </EmptyBlock>
+        </CommandEmpty>
         <CommandGroup heading="Suggestions">
-          <CommandItem>
-            <Bot className="mr-2 h-4 w-4" />
-            <span>Copilots</span>
-          </CommandItem>
-          <CommandItem>
-            <HelpCircle className="mr-2 h-4 w-4" />
-            <span>Help</span>
-          </CommandItem>
-          <CommandItem>
-            <FileStack className="mr-2 h-4 w-4" />
-            <span>Documentation</span>
-          </CommandItem>
+          <Link href="/">
+            <CommandItem>
+              <Bot className="mr-2 h-4 w-4" />
+              <span>Copilots</span>
+            </CommandItem>
+          </Link>
+          <Link href="https://docs.opencopilot.so" target="_blank">
+            <CommandItem>
+              <HelpCircle className="mr-2 h-4 w-4" />
+              <span>Help</span>
+            </CommandItem>
+          </Link>
+          <Link href="https://docs.opencopilot.so" target="_blank">
+            <CommandItem>
+              <FileStack className="mr-2 h-4 w-4" />
+              <span>Documentation</span>
+            </CommandItem>
+          </Link>
         </CommandGroup>
         <CommandSeparator />
-        <CommandGroup heading="Settings">
-          <CommandItem>
-            <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CreditCard className="mr-2 h-4 w-4" />
-            <span>Billing</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
-          </CommandItem>
+        <CommandGroup heading="Copilots">
+          {copilots?.data.map((copilot) => (
+            <CommandItem
+              key={copilot.id}
+              onSelect={() => push("/copilot/" + copilot.id)}
+            >
+              <User className="mr-2 h-4 w-4" />
+              <span>
+                {copilot.name}
+                <span className="hidden">{copilot.id}</span>
+              </span>
+            </CommandItem>
+          ))}
         </CommandGroup>
       </CommandList>
     </CommandDialog>
