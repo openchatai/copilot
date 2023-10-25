1cfb4076f6f44cd4743a3d87edbfaec8d0635362
david.smith <david.smith@hashicorp.com>
2023-10-25 22:17:07 +0300

    refactoring code for scalability

--- llm-server/routes/root_service.py
+++ llm-server/routes/root_service.py
@@ -1,14 +1,13 @@
-import json
 import os
-from typing import Dict, Any, cast, Optional
+from typing import Dict, Any, cast, Optional, List, Tuple
 
 import logging
 from dotenv import load_dotenv
+from opencopilot_types.workflow_type import WorkflowDataType
 from routes.workflow.typings.run_workflow_input import WorkflowData
 from routes.workflow.utils import (
     run_workflow,
     check_workflow_in_store,
-    fetch_swagger_text,
     hasSingleIntent,
     create_workflow_from_operation_ids,
 )
@@ -18,13 +17,12 @@ from dotenv import load_dotenv
 from typing import Dict, Any, cast
 from utils.db import Database
 import json
-from api_caller.base import try_to_match_and_call_api_endpoint
 from models.repository.chat_history_repo import create_chat_history
 from utils.process_app_state import process_state
 from prance import ResolvingParser
 from utils.vector_db.add_workflow import add_workflow_data_to_qdrant
 from uuid import uuid4
-
+from langchain.docstore.document import Document
 
 db_instance = Database()
 mongo = db_instance.get_db()
@@ -42,15 +40,81 @@ FAILED_TO_CALL_API_ENDPOINT = "Failed to call or map API endpoint"
 
 
 def handle_request(data: Dict[str, Any]) -> Any:
-    text: str = cast(str, data.get("text"))
+    (
+        text,
+        swagger_url,
+        session_id,
+        base_prompt,
+        headers,
+        server_base_url,
+        app,
+    ) = extract_data(data)
+
+    log_user_request(text)
+
+    check_required_fields(base_prompt, text, swagger_url)
+
+    swagger_doc = get_swagger_doc(swagger_url)
+
+    try:
+        current_state = process_state(app, headers)
+        document, score = check_workflow_in_store(text, swagger_url)
+
+        if document:
+            return handle_existing_workflow(
+                document,
+                text,
+                headers,
+                server_base_url,
+                swagger_url,
+                app,
+                swagger_doc,
+                session_id,
+            )
+
+        bot_response = hasSingleIntent(swagger_doc, text, session_id, current_state)
+
+        if len(bot_response.ids) >= 1:
+            return handle_api_calls(
+                bot_response.ids,
+                swagger_doc,
+                text,
+                headers,
+                server_base_url,
+                swagger_url,
+                app,
+                session_id,
+                document,
+            )
+
+        elif len(bot_response.ids) == 0:
+            return handle_no_api_call(
+                swagger_url, session_id, text, bot_response.bot_message
+            )
+
+    except Exception as e:
+        return handle_exception(e)
+
+
+# Helper Functions
+
+
+def extract_data(data: Dict[str, Any]) -> Tuple:
+    text = cast(str, data.get("text"))
     swagger_url = cast(str, data.get("swagger_url", ""))
     session_id = cast(str, data.get("session_id", ""))
     base_prompt = data.get("base_prompt", "")
     headers = data.get("headers", {})
     server_base_url = cast(str, data.get("server_base_url", ""))
     app = headers.get("X-App-Name") or None
+    return text, swagger_url, session_id, base_prompt, headers, server_base_url, app
 
+
+def log_user_request(text: str) -> None:
     logging.info("[OpenCopilot] Got the following user request: {}".format(text))
+
+
+def check_required_fields(base_prompt: str, text: str, swagger_url: str) -> None:
     for required_field, error_msg in [
         ("base_prompt", BASE_PROMPT_REQUIRED),
         ("text", TEXT_REQUIRED),
@@ -59,86 +123,88 @@ def handle_request(data: Dict[str, Any]) -> Any:
         if not locals()[required_field]:
             raise Exception(error_msg)
 
-    swagger_doc: Dict[str, Any] = mongo.swagger_files.find_one(
+
+def get_swagger_doc(swagger_url: str) -> ResolvingParser:
+    swagger_doc: Optional[Dict[str, Any]] = mongo.swagger_files.find_one(
         {"meta.swagger_url": swagger_url}, {"meta": 0, "_id": 0}
     )
 
-    # need to test if prance works with document saved in mongodb
-
     if swagger_url.startswith("http:") or swagger_url.startswith("https:"):
-        swagger_doc = ResolvingParser(url=swagger_url)
+        return ResolvingParser(url=swagger_url)
     elif swagger_doc:
-        swagger_doc = ResolvingParser(spec_string=swagger_doc)
+        return ResolvingParser(spec_string=swagger_doc)
     else:
-        swagger_doc = ResolvingParser(url=shared_folder + swagger_url)
-    try:
-        logging.info(
-            "[OpenCopilot] Trying to figure out if the user request require 1) APIs calls 2) If yes how many "
-            "of them"
+        return ResolvingParser(url=shared_folder + swagger_url)
+
+
+def handle_existing_workflow(
+    document: Document,  # lagnchaing
+    text: str,
+    headers: Dict[str, Any],
+    server_base_url: str,
+    swagger_url: str,
+    app: str,
+    swagger_doc: ResolvingParser,
+    session_id: str,
+) -> Dict[str, Any]:
+    _workflow = mongo.workflows.find_one(
+        {"_id": ObjectId(document.metadata["workflow_id"])}
+    )
+    output = run_workflow(
+        _workflow,
+        swagger_doc,
+        WorkflowData(text, headers, server_base_url, swagger_url, app),
+        app,
+    )
+    create_chat_history(swagger_url, session_id, True, text)
+    create_chat_history(
+        swagger_url, session_id, False, output["response"] or output["error"]
+    )
+    return output
+
+
+def handle_api_calls(
+    ids: List[str],
+    swagger_doc: ResolvingParser,
+    text: str,
+    headers: Dict[str, Any],
+    server_base_url: str,
+    swagger_url: str,
+    app: str,
+    session_id: str,
+    document: Optional[Document],
+) -> Dict[str, Any]:
+    _workflow = create_workflow_from_operation_ids(ids, swagger_doc, text)
+    output = run_workflow(
+        _workflow,
+        swagger_doc,
+        WorkflowData(text, headers, server_base_url, swagger_url, app),
+        app,
+    )
+
+    if document is None:
+        mongo.auto_gen_workflows.insert_one(
+            {"workflow": _workflow, "swagger_url": swagger_url}
         )
-        current_state = process_state(app, headers)
+        add_workflow_data_to_qdrant(str(uuid4()), _workflow, swagger_url)
 
-        logging.info(f"Received app configuration: {app}")
-        bot_response = hasSingleIntent(swagger_doc, text, session_id, current_state)
-        if len(bot_response.ids) >= 1:
-            logging.info(
-                "[OpenCopilot] Apparently, the user request require calling one or more API endpoint "
-                "to get the job done"
-            )
+    create_chat_history(swagger_url, session_id, True, text)
+    create_chat_history(
+        swagger_url, session_id, False, output["response"] or output["error"]
+    )
+    return output
 
-            # check workflow in mongodb, if present use that, else ask planner to create a workflow based on summaries
-            # then call run_workflow on that
-            (document, score) = check_workflow_in_store(text, swagger_url)
 
-            _workflow = None
-            if document:
-                _workflow = mongo.workflows.find_one(
-                    {"_id": ObjectId(document.metadata["workflow_id"])}
-                )
-            else:
-                _workflow = create_workflow_from_operation_ids(
-                    bot_response.ids, swagger_doc, text
-                )
-            output = run_workflow(
-                _workflow,
-                swagger_doc,
-                WorkflowData(text, headers, server_base_url, swagger_url, app),
-                app,
-            )
+def handle_no_api_call(
+    swagger_url: str, session_id: str, text: str, bot_message: str
+) -> Dict[str, str]:
+    create_chat_history(swagger_url, session_id, True, text)
+    create_chat_history(swagger_url, session_id, False, bot_message)
+    return {"response": bot_message}
 
-            # if a flow doesnot already exist and run_workflow was successful, include this
-            if document is None:
-                mongo.auto_gen_workflows.insert_one(
-                    {"workflow": _workflow, "swagger_url": swagger_url}
-                )
-                add_workflow_data_to_qdrant(str(uuid4()), _workflow, swagger_url)
 
-            create_chat_history(swagger_url, session_id, True, text)
-            # bot response
-            create_chat_history(
-                swagger_url, session_id, False, output["response"] or output["error"]
-            )
-
-            return output
-
-        elif len(bot_response.ids) == 0:
-            logging.info("[OpenCopilot] The user request doesnot require an api call")
-
-            create_chat_history(swagger_url, session_id, True, text)
-            # bot response
-            create_chat_history(
-                swagger_url, session_id, False, bot_response.bot_message
-            )
-            return {"response": bot_response.bot_message}
-
-        else:
-            logging.info(
-                "[OpenCopilot] The user request can be handled in single API call"
-            )
-
-    except Exception as e:
-        logging.info(
-            "[OpenCopilot] Something went wrong when try to get how many calls is required"
-        )
-
-        return {"response": None, "error": str(e)}
+def handle_exception(e: Exception) -> Dict[str, Any]:
+    logging.info(
+        "[OpenCopilot] Something went wrong when trying to get how many calls are required"
+    )
+    return {"response": None, "error": str(e)}
