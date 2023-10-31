bdf2699b9159396cbe2ce6146e7fa754c6d2e572
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-11-01 01:50:01 +0200

    refactor: Remove unused imports and variables + utilize `getData` func. from latest `opencopilot-flows-editor`'s API.

--- dashboard/app/(copilot)/copilot/[copilot_id]/flows/page.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/flows/page.tsx
@@ -1,24 +1,18 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   CodePreview,
   Controller,
   FlowArena,
   useController,
   transformPaths,
-  trasnformEndpointNodesData,
-  transformaEndpointToNode,
 } from "@openchatai/copilot-flows-editor";
 import { HeaderShell } from "@/components/domain/HeaderShell";
 import { useCopilot } from "../../_context/CopilotProvider";
 import useSwr, { mutate } from "swr";
 import { getSwaggerByBotId } from "@/data/swagger";
 import { Button } from "@/components/ui/button";
-import {
-  createWorkflowByBotId,
-  deleteWorkflowById,
-  getWorkflowById,
-} from "@/data/flow";
+import { createWorkflowByBotId, deleteWorkflowById } from "@/data/flow";
 import { useRouter } from "next/navigation";
 import { toast } from "@/components/ui/use-toast";
 import _ from "lodash";
@@ -29,7 +23,7 @@ function Header() {
   const [isEditing, workflow_id] = useIsEditing();
   const { replace } = useRouter();
   const { id: copilotId, name: copilotName } = useCopilot();
-  const { loadPaths, state, reset: resetFlowEditor } = useController();
+  const { loadPaths, reset: resetFlowEditor, getData } = useController();
 
   const [loading, setLoading] = useState(false);
   const { isLoading: isSwaggerLoading, mutate: mutateSwagger } = useSwr(
@@ -64,46 +58,34 @@ function Header() {
   }
   async function handleCreate() {
     if (isEditing) return;
-    const firstFlow = _.first(state.flows);
+    const firstFlow = _.first(getData().flows);
     if (firstFlow) {
       setLoading(true);
-      const steps = trasnformEndpointNodesData(firstFlow?.steps || []);
-      const { data } = await createWorkflowByBotId(copilotId, {
-        opencopilot: "0.1",
-        info: {
-          title: "My OpenCopilot definition",
-          version: "1.0.0",
-        },
-        flows: [
-          {
-            name: firstFlow?.name,
-            description: firstFlow?.description,
-            steps: steps.map((step) => ({
-              operation: "call",
-              stepId: step.id,
-              open_api_operation_id: step.operationId,
-            })),
-            requires_confirmation: true,
-            on_success: [{}],
-            on_failure: [{}],
-          },
-        ],
-      });
-      if (data.workflow_id) {
+      try {
+        const { data } = await createWorkflowByBotId(copilotId, getData());
+        if (data.workflow_id) {
+          toast({
+            title: "Workflow created",
+            description: "Your workflow has been created.",
+            variant: "success",
+          });
+          replace(
+            `/copilot/${copilotId}/flows/?workflow_id=${data.workflow_id}`,
+            {
+              scroll: false,
+            },
+          );
+          mutate(copilotId + "/workflows");
+        }
+      } catch (error) {
         toast({
-          title: "Workflow created",
-          description: "Your workflow has been created.",
-          variant: "success",
+          title: "Workflow not created",
+          description: "Something went wrong.",
+          variant: "destructive",
         });
-        replace(
-          `/copilot/${copilotId}/flows/?workflow_id=${data.workflow_id}`,
-          {
-            scroll: false,
-          },
-        );
-        mutate(copilotId + "/workflows");
+      } finally {
+        setLoading(false);
       }
-      setLoading(false);
     } else {
       toast({
         title: "Workflow not created",
@@ -149,79 +131,6 @@ function Header() {
     </HeaderShell>
   );
 }
-const flows = [
-  {
-    createdAt: 1698626518815,
-    name: "New Flow",
-    description: "A flow that does something",
-    focus: true,
-    steps: [
-      {
-        width: 250,
-        height: 115,
-        id: "loc6dg5rzvqwjr3n47",
-        type: "endpointNode",
-        data: {
-          path: "/v2.0/metering/metering-label-rules",
-          method: "get",
-          description:
-            "Lists a summary of all l3 metering label rules belonging to the specified tenant.\n",
-          operationId: "listMeteringLabelRules",
-          produces: ["application/json"],
-          responses: {
-            "200": {
-              description: "200 response",
-              examples: {
-                "application/json":
-                  '{\n    "metering_label_rules": [\n        {\n            "remote_ip_prefix": "20.0.0.0/24",\n            "direction": "ingress",\n            "metering_label_id": "e131d186-b02d-4c0b-83d5-0c0725c4f812",\n            "id": "9536641a-7d14-4dc5-afaf-93a973ce0eb8",\n            "excluded": false\n        },\n        {\n            "remote_ip_prefix": "10.0.0.0/24",\n            "direction": "ingress",\n            "metering_label_id": "e131d186-b02d-4c0b-83d5-0c0725c4f812",\n            "id": "ffc6fd15-40de-4e7d-b617-34d3f7a93aec",\n            "excluded": false\n        }\n    ]\n}',
-              },
-            },
-          },
-          summary: "List metering label rules",
-        },
-        draggable: false,
-        position: {
-          x: 0,
-          y: 0,
-        },
-        positionAbsolute: {
-          x: 0,
-          y: 0,
-        },
-      },
-      {
-        id: "loc6dgvm0opcqfggdvba",
-        type: "endpointNode",
-        data: {
-          path: "/v2.0/metering/metering-label-rules",
-          method: "post",
-          description: "Creates a l3 metering label rule.\n",
-          operationId: "createMeteringLabelRule",
-          produces: ["application/json"],
-          responses: {
-            "201": {
-              description: "201 response",
-              examples: {
-                "application/json":
-                  '{\n    "metering_label_rule": {\n        "remote_ip_prefix": "10.0.1.0/24",\n        "direction": "ingress",\n        "metering_label_id": "e131d186-b02d-4c0b-83d5-0c0725c4f812",\n        "id": "00e13b58-b4f2-4579-9c9c-7ac94615f9ae",\n        "excluded": false\n    }\n}',
-              },
-            },
-          },
-          summary: "Create metering label rule",
-        },
-        draggable: false,
-        position: {
-          x: 0,
-          y: 150,
-        },
-        width: 250,
-        height: 99,
-      },
-    ],
-    id: "loc6de743yp05gf6wqa",
-    updatedAt: 1698626522353,
-  },
-];
 export default function FlowsPage({
   params: { copilot_id },
 }: {
@@ -230,22 +139,6 @@ export default function FlowsPage({
   const [isEditing, workflow_id] = useIsEditing();
   console.log({ isEditing, workflow_id });
   const [state, setState] = useState<any>();
-  useSwr(workflow_id, getWorkflowById, {
-    onSuccess: (data) => {
-      const workflow = data.data;
-      const s = {
-        ...state,
-        flows: workflow.flows.map((flow) => ({
-          ...flow,
-          steps: transformaEndpointToNode(flow?.steps || []),
-        })),
-      };
-      setState(s);
-    },
-  });
-  useEffect(() => {
-    console.log("state changed", state);
-  }, [state]);
   return (
     // @ts-ignore
     <Controller maxFlows={1} initialState={state} onChange={setState}>
