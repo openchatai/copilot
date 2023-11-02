88f253e33b893bd6762fe82d1069426ef5e719aa
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-11-02 02:14:55 +0200

    feat(add-data-source): Add URL and File data source options

--- dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/AddDataSource.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/AddDataSource.tsx
@@ -9,34 +9,98 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {
   Dialog,
+  DialogCancel,
   DialogContent,
-  DialogHeader,
   DialogFooter,
-  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
+import { atom, useAtom, useSetAtom } from "jotai";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { Textarea } from "@/components/ui/textarea";
+import { DropZone } from "@/components/domain/DropZone";
+type DialogTypes = "url" | "file" | null;
+const activeDialog = atom<DialogTypes>(null);
 
 function AddUrlDataSource() {
+  const [dialog, setDialog] = useAtom(activeDialog);
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <DropdownMenuItem>URL(s)</DropdownMenuItem>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>Add URL(s)</DialogHeader>
+    <Dialog
+      open={dialog === "url"}
+      onOpenChange={(op) => {
+        if (!op) setDialog(null);
+      }}
+    >
+      <DialogContent withClose>
+        <DialogHeader>
+          <DialogTitle>Add URL(s)</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>add one or more URLs to scrape</DialogDescription>
+        <Textarea minRows={4} placeholder="https://example.com/page" />
+        <DialogFooter>
+          <DialogCancel asChild>
+            <Button variant="ghost">Cancel</Button>
+          </DialogCancel>
+          <Button variant="default">Add</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function AddFileDataSource() {
+  const [dialog, setDialog] = useAtom(activeDialog);
+  return (
+    <Dialog
+      open={dialog === "file"}
+      onOpenChange={(op) => {
+        if (!op) setDialog(null);
+      }}
+    >
+      <DialogContent withClose>
+        <DialogHeader>
+          <DialogTitle>
+            Add File
+            <span className="text-sm text-muted-foreground">
+              {" "}
+              (CSV, JSON, XML, PDF)
+            </span>
+          </DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          add a file to upload and scrape data from.
+        </DialogDescription>
+        <DropZone />
+        <DialogFooter>
+          <DialogCancel asChild>
+            <Button variant="ghost">Cancel</Button>
+          </DialogCancel>
+          <Button variant="default">Add</Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
 }
 
 export function AddDataSource() {
+  const setDialog = useSetAtom(activeDialog);
   return (
-    <DropdownMenu modal={false}>
-      <DropdownMenuTrigger asChild>
-        <Button>Add Data Source</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <AddUrlDataSource />
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button>Add Data Source</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="min-w-trigger-anchor [&>*]:text-base">
+          <DropdownMenuItem onClick={() => setDialog("url")}>
+            URL(s)
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setDialog("file")}>
+            File
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AddUrlDataSource />
+      <AddFileDataSource />
+    </>
   );
 }
--- dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/KnowledgeTable.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/KnowledgeTable.tsx
@@ -1,5 +1,69 @@
+"use client";
 import React from "react";
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  TableHead,
+  TableHeader,
+} from "@/components/ui/table";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Minus, Trash2 } from "lucide-react";
 export function KnowledgeTable() {
-  return <div>KnowledgeTable</div>;
+  return (
+    <HoverCard open>
+      <Table wrapperClassName="rounded-none">
+        <TableHeader>
+          <TableRow>
+            <TableHead role="checkbox" className="w-2 px-8">
+              <Checkbox CheckedIcon={Minus} />
+            </TableHead>
+            <TableHead className="px-8 text-accent-foreground">Name</TableHead>
+            <TableHead className="px-8">Type</TableHead>
+            <TableHead className="px-8">Status</TableHead>
+            <TableHead className="px-8">Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <HoverCardTrigger asChild>
+          <TableBody className="odd:bg-white">
+            <TableRow>
+              <TableCell role="checkbox" className="w-2 pl-8">
+                <Checkbox />
+              </TableCell>
+              <TableCell className="px-8">Cell 1</TableCell>
+              <TableCell className="px-8">Cell 2</TableCell>
+              <TableCell className="px-8">Cell 3</TableCell>
+              <TableCell className="px-8">Cell 3</TableCell>
+            </TableRow>
+          </TableBody>
+        </HoverCardTrigger>
+      </Table>
+      <HoverCardContent
+        side="bottom"
+        className="h-14 w-[400px] rounded-xl border-none bg-[#33373A] p-2 text-white"
+      >
+        <div className="ml-3 flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <button className="flex-center h-4 w-4 rounded bg-[#5D6264] text-white">
+              <Minus className="h-3 w-3" />
+            </button>
+            <div className="text-sm text-muted-foreground">
+              <span>
+                <span className="text-white">3</span> documents selected
+              </span>
+            </div>
+          </div>
+          <button className="flex-center rounded-lg bg-[#5D6264] p-3 text-white">
+            <Trash2 className="h-4 w-4" />
+          </button>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  );
 }
