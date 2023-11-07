70991da5d001a8fc62cd85dc7f83cc0cbd391ce7
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-11-07 18:49:51 +0200

    Add table functionality to KnowledgeTable component

--- dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/KnowledgeTable.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/KnowledgeTable.tsx
@@ -14,33 +14,179 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Minus, Trash2 } from "lucide-react";
+import { Minus, Trash2, ChevronsUpDown } from "lucide-react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  RowSelectionState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import _ from "lodash";
+import { timeSince } from "@/lib/timesince";
+import { Button } from "@/components/ui/button";
+export type DataSources = {
+  id: string;
+  name: string;
+  type: string;
+  status: "pending" | "error" | "success";
+  date: Date | number | string;
+};
+const columns: ColumnDef<DataSources>[] = [
+  {
+    id: "select",
+    header({ table }) {
+      return (
+        <Checkbox
+          CheckedIcon={Minus}
+          checked={table.getIsAllRowsSelected()}
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        />
+      );
+    },
+    cell({ row }) {
+      return (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+        />
+      );
+    },
+    enableSorting: false,
+    enableHiding: false,
+    accessorKey: "id",
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => (
+      <div className="flex items-center gap-2">
+        <span>Name</span>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          <ChevronsUpDown className="h-4 w-4" />
+        </Button>
+      </div>
+    ),
+    cell: ({ row }) => (
+      <span className="text-accent-foreground">{row.getValue("name")}</span>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("status")}</div>
+    ),
+  },
+  {
+    accessorKey: "type",
+    header: "Type",
+    cell: ({ row }) => row.getValue("type"),
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+    cell: ({ row }) => <span>{timeSince(row.getValue("date"))} ago</span>,
+  },
+];
+const data: DataSources[] = [
+  {
+    id: "1",
+    date: "2021-10-01T00:00:00.000Z",
+    name: "Your Website title",
+    status: "pending",
+    type: "URL",
+  },
+];
 export function KnowledgeTable() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState<RowSelectionState>({});
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+  const selection = table.getSelectedRowModel();
   return (
-    <HoverCard open>
+    <HoverCard open={!_.isEmpty(selection.rows)}>
       <Table wrapperClassName="rounded-none">
         <TableHeader>
-          <TableRow>
-            <TableHead role="checkbox" className="w-2 px-8">
-              <Checkbox CheckedIcon={Minus} />
-            </TableHead>
-            <TableHead className="px-8 text-accent-foreground">Name</TableHead>
-            <TableHead className="px-8">Type</TableHead>
-            <TableHead className="px-8">Status</TableHead>
-            <TableHead className="px-8">Date</TableHead>
-          </TableRow>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} className="px-8">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
         </TableHeader>
+
         <HoverCardTrigger asChild>
           <TableBody className="odd:bg-white">
-            <TableRow>
-              <TableCell role="checkbox" className="w-2 pl-8">
-                <Checkbox />
-              </TableCell>
-              <TableCell className="px-8">Cell 1</TableCell>
-              <TableCell className="px-8">Cell 2</TableCell>
-              <TableCell className="px-8">Cell 3</TableCell>
-              <TableCell className="px-8">Cell 3</TableCell>
-            </TableRow>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell
+                      key={cell.id}
+                      className="px-8 data-[role=checkbox]:w-2 data-[role=checkbox]:pl-8"
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-16 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </HoverCardTrigger>
       </Table>
@@ -50,12 +196,18 @@ export function KnowledgeTable() {
       >
         <div className="ml-3 flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <button className="flex-center h-4 w-4 rounded bg-[#5D6264] text-white">
+            <button
+              className="flex-center h-4 w-4 rounded bg-[#5D6264] text-white"
+              onClick={() => {
+                table.setRowSelection({});
+              }}
+            >
               <Minus className="h-3 w-3" />
             </button>
             <div className="text-sm text-muted-foreground">
               <span>
-                <span className="text-white">3</span> documents selected
+                <span className="text-white">{selection.rows.length}</span>{" "}
+                documents selected
               </span>
             </div>
           </div>
