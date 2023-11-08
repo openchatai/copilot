4fe4981d914239fcba65a1b84b28b431d1b4ba12
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-11-08 03:10:02 +0200

    use AlertDialog instead of Dialog.

--- dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/AddDataSource.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/knowledge/_parts/AddDataSource.tsx
@@ -7,83 +7,97 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
-import {
-  Dialog,
-  DialogCancel,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import { atom, useAtom, useSetAtom } from "jotai";
-import { DialogDescription } from "@radix-ui/react-dialog";
 import { Textarea } from "@/components/ui/textarea";
 import { DropZone } from "@/components/domain/DropZone";
+import { useHotkeys } from "react-hotkeys-hook";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogDescription,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 type DialogTypes = "url" | "file" | null;
 const activeDialog = atom<DialogTypes>(null);
 
 function AddUrlDataSource() {
   const [dialog, setDialog] = useAtom(activeDialog);
   return (
-    <Dialog
+    <AlertDialog
       open={dialog === "url"}
       onOpenChange={(op) => {
         if (!op) setDialog(null);
       }}
     >
-      <DialogContent withClose>
-        <DialogHeader>
-          <DialogTitle>Add URL(s)</DialogTitle>
-        </DialogHeader>
-        <DialogDescription>add one or more URLs to scrape</DialogDescription>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Add URL(s)</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogDescription>
+          add one or more URLs to scrape
+        </AlertDialogDescription>
         <Textarea minRows={4} placeholder="https://example.com/page" />
-        <DialogFooter>
-          <DialogCancel asChild>
+        <AlertDialogFooter>
+          <AlertDialogCancel asChild>
             <Button variant="ghost">Cancel</Button>
-          </DialogCancel>
+          </AlertDialogCancel>
           <Button variant="default">Add</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
 function AddFileDataSource() {
   const [dialog, setDialog] = useAtom(activeDialog);
   return (
-    <Dialog
+    <AlertDialog
       open={dialog === "file"}
       onOpenChange={(op) => {
         if (!op) setDialog(null);
       }}
     >
-      <DialogContent withClose>
-        <DialogHeader>
-          <DialogTitle>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
             Add File
             <span className="text-sm text-muted-foreground">
               {" "}
-              (CSV, JSON, XML, PDF)
+              (CSV, JSON, XML, PDF, HTML)
             </span>
-          </DialogTitle>
-        </DialogHeader>
-        <DialogDescription>
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogDescription>
           add a file to upload and scrape data from.
-        </DialogDescription>
-        <DropZone />
-        <DialogFooter>
-          <DialogCancel asChild>
+        </AlertDialogDescription>
+        <DropZone
+          maxFiles={1}
+          accept={{
+            json: ["application/json"],
+            html: ["text/html"],
+            csv: ["text/csv"],
+            xml: ["text/xml"],
+            pdf: ["application/pdf"],
+          }}
+        />
+        <AlertDialogFooter>
+          <AlertDialogCancel asChild>
             <Button variant="ghost">Cancel</Button>
-          </DialogCancel>
+          </AlertDialogCancel>
           <Button variant="default">Add</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
 export function AddDataSource() {
   const setDialog = useSetAtom(activeDialog);
+  useHotkeys("shift+u", () => setDialog("url"));
+  useHotkeys("shift+f", () => setDialog("file"));
   return (
     <>
       <DropdownMenu modal={false}>
@@ -91,11 +105,23 @@ export function AddDataSource() {
           <Button>Add Data Source</Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="min-w-trigger-anchor [&>*]:text-base">
-          <DropdownMenuItem onClick={() => setDialog("url")}>
+          <DropdownMenuItem
+            onClick={() => setDialog("url")}
+            className="flex items-center justify-between"
+          >
             URL(s)
+            <span className="text-xs font-medium text-secondary-foreground">
+              shift + u
+            </span>
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setDialog("file")}>
+          <DropdownMenuItem
+            onClick={() => setDialog("file")}
+            className="flex items-center justify-between"
+          >
             File
+            <span className="text-xs font-medium text-secondary-foreground">
+              shift + f
+            </span>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
