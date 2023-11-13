f693a717a051467dec17d69b1f05c354e564b324
ah7255703 <ah7255703@gmail.com>
2023-11-13 20:41:15 +0200

    Add validation error border to Input component

--- dashboard/components/ui/input.tsx
+++ dashboard/components/ui/input.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -12,6 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           "h-10 w-full m-0 bg-white shadow-sm min-w-fit rounded-md border block focus:outline-none border-border bg-background px-3 py-2 text-sm ring-offset-accent file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-primary transition-colors disabled:cursor-not-allowed disabled:opacity-50",
+          "data-[valid=false]:border-destructive",
           className
         )}
         ref={ref}
