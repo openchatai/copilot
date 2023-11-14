e2c74606d1177cd4c92b443b0c729de989d79a7a
ah7255703 <ah7255703@gmail.com>
2023-11-15 01:40:09 +0200

    Add Copilot widget CSS import and pass token as prop to Widget component

--- dashboard/app/(copilot)/copilot/[copilot_id]/CopilotWidget.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/CopilotWidget.tsx
@@ -1,17 +1,18 @@
-import "@openchatai/copilot-widget/index.css";
 import {
     CopilotWidget,
     Root
 } from '@openchatai/copilot-widget';
-
-
-export default function Widget() {
+export default function Widget({
+    token
+}: {
+    token: string
+}) {
     return <Root
         options={{
             apiUrl: "http://localhost:8888/backend/api",
-            token: "",
+            token,
             initialMessage: "Hey Pal!",
-            headers: {}
+            headers: {},
         }}
     >
         <div className="[&>div]:static [&>div]:!max-h-full [&>div]:!h-full h-full overflow-hidden border-border border rounded-lg">
