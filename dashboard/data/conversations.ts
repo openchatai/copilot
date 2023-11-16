1a681fcae8356de37137c50cb8c1aed5dbaf93cb
ah7255703 <ah7255703@gmail.com>
2023-11-16 20:37:27 +0200

    Refactor conversation types and add session retrieval by bot ID

--- dashboard/data/conversations.ts
+++ dashboard/data/conversations.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "http://localhost:8888/backend/chat",
 });
-export type ConversationType = {
+export type ChatMessageType = {
   id: string;
   chatbot_id: string;
   session_id: string;
@@ -11,7 +11,27 @@ export type ConversationType = {
   created_at: string;
 };
 
+// http://localhost:8888/backend/chat/sessions/:sessionId/chats
 export async function getConversationBySessionId(sessionId: string) {
   if (!sessionId) return;
-  return await instance.get<ConversationType[]>(`/sessions/${sessionId}/chats`);
+  return instance.get<ChatMessageType[]>(`/sessions/${sessionId}/chats`);
+}
+
+export type ConversationType = {
+  first_message: {
+    id: number;
+    chatbot_id: string;
+    created_at: string;
+    from_user: boolean;
+    message: string;
+    session_id: string;
+    updated_at: string;
+  };
+  session_id: string;
+}
+
+// http://localhost:8888/backend/chat/b/:bot_id/chat_sessions
+export async function getSessionsByBotId(bot_id: string) {
+  if (!bot_id) return;
+  return instance.get<ConversationType[]>(`/b/${bot_id}/chat_sessions`);
 }
