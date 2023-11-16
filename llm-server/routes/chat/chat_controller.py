dee21acf207f22931d6a15600dc0ea8bf830dc6c
shanurrahman <shanur.cse.nitap@gmail.com>
2023-11-16 15:27:36 +0300

    return all unique sessions for given bot id

--- llm-server/routes/chat/chat_controller.py
+++ llm-server/routes/chat/chat_controller.py
@@ -1,4 +1,7 @@
-from models.repository.chat_history_repo import get_all_chat_history_by_session_id
+from models.repository.chat_history_repo import (
+    get_all_chat_history_by_session_id,
+    get_unique_sessions_with_first_message_by_bot_id,
+)
 from flask import Blueprint, request, jsonify
 from utils.db import Database
 from flask import Flask, request, jsonify, Blueprint, request, Response
@@ -32,3 +35,17 @@ def get_session_chats(session_id: str) -> Response:
         )
 
     return jsonify(chats_filtered)
+
+
+# unique_session_ids = get_unique_session_ids(session)
+
+
+@chat_workflow.route("/b/<bot_id>/chat_sessions", methods=["GET"])
+def get_chat_sessions(bot_id: str) -> Response:
+    limit = request.args.get("limit", 20)
+    offset = request.args.get("offset", 0)
+    chat_history_sessions = get_unique_sessions_with_first_message_by_bot_id(
+        bot_id, limit, offset
+    )
+
+    return chat_history_sessions
