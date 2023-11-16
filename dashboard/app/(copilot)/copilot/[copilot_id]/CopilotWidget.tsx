769f01ab5ea0d034ea74aae6eb9d0b6c205bd7ef
ah7255703 <ah7255703@gmail.com>
2023-11-16 19:04:57 +0200

    Add error boundary component to CopilotWidget.tsx and import react-error-boundary package

--- dashboard/app/(copilot)/copilot/[copilot_id]/CopilotWidget.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/CopilotWidget.tsx
@@ -1,27 +1,39 @@
+import { Button } from '@/components/ui/button';
 import {
     CopilotWidget,
     Root
 } from '@openchatai/copilot-widget';
+import { ErrorBoundary } from "react-error-boundary";
 export default function Widget({
     token
 }: {
     token: string
 }) {
-    return <Root
-        options={{
-            apiUrl: "http://localhost:8888/backend/api",
-            defaultOpen: true,
-            token,
-            initialMessage: "Hey Pal!",
-            headers: {
-                "X-Copilot": "copilot"
-            },
-        }}
-    >
-        <div className="[&>div]:static [&>div]:!max-h-full [&>div]:!h-full h-full overflow-hidden [&>div]:!border-border [&>div]:!border rounded-lg">
-            <CopilotWidget
-                triggerSelector="#triggerSelector"
-            />
+    return <ErrorBoundary FallbackComponent={({ resetErrorBoundary }) => {
+        return <div className="flex flex-col items-center justify-center h-full">
+            <div className="flex flex-col gap-3 items-center justify-center">
+                <h1 className="text-2xl font-semibold text-gray-700">Something went wrong</h1>
+                <Button onClick={() => resetErrorBoundary()} variant='destructive'>Try again</Button>
+            </div>
         </div>
-    </Root>
+    }}>
+
+        <Root
+            options={{
+                apiUrl: "http://localhost:8888/backend/api",
+                defaultOpen: true,
+                token,
+                initialMessage: "Hey Pal!",
+                headers: {
+                    "X-Copilot": "copilot"
+                },
+            }}
+        >
+            <div className="[&>div]:static [&>div]:!max-h-full [&>div]:!h-full h-full overflow-hidden [&>div]:!border-border [&>div]:!border rounded-lg">
+                <CopilotWidget
+                    triggerSelector="#triggerSelector"
+                />
+            </div>
+        </Root>
+    </ErrorBoundary>
 }
