90a22d000a0a8f6e42e58e15f2a521075519121f
gharbat <arbioun@gmail.com>
2023-11-19 13:15:19 +0100

    Add a new API to store the swagger file

--- llm-server/routes/copilot/copilot_controller.py
+++ llm-server/routes/copilot/copilot_controller.py
@@ -18,3 +18,30 @@ def index():
 
     return jsonify([chatbot.to_dict() for chatbot in chatbots])  # Convert chatbots to dictionaries if needed
 
+
+@copilot_workflow.route('/copilot/swagger', methods=['POST'])
+def handle_swagger_file():
+    if 'swagger_file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files['swagger_file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    if file:  # Add your file validation logic here if needed
+        filename = secure_filename(str(uuid.uuid4()) + ".json")
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+        # Create Chatbot instance and save to DB (adjust as per your application logic)
+        chatbot = Chatbot(name=request.form.get('name'),
+                          swagger_url=filename,
+                          prompt_message=request.form.get('prompt_message'),
+                          website=request.form.get('website'))
+        # Add your DB commit logic here
+
+        server_url = f"{LLM_SERVER_ENDPOINT}/swagger_api/init/b/{chatbot.id}"
+        response = requests.post(server_url, json={'swagger_url': filename})
+
+        return jsonify({
+            'file_name': filename,
+            'chatbot': chatbot.to_dict()  # Convert chatbot to dictionary
+        })
