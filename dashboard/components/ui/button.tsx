a915f299135ad426d2bde1e1c22e874362ef4334
ah7255703 <ah7255703@gmail.com>
2023-11-21 01:26:46 +0200

    Refactor button and select components for better styling and responsiveness

--- dashboard/components/ui/button.tsx
+++ dashboard/components/ui/button.tsx
@@ -26,6 +26,7 @@ const buttonVariants = cva(
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
         fluid: "w-full h-10",
+        fit:"w-fit h-fit p-2"
       },
     },
     defaultVariants: {
