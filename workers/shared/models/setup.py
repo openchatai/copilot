85e9e907ff0b4e40ca0cda6e3c85f603d1822a3c
gharbat <arbioun@gmail.com>
2023-11-24 21:46:39 +0100

    Bump up db models version

--- workers/shared/models/setup.py
+++ workers/shared/models/setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as readme_file:
 
 setup(
     name="opencopilot_db",
-    version="2.0.0",
+    version="3.0.0",
     packages=find_packages(),
     install_requires=[
         "sqlalchemy",
