a794c811aba9416b3bfb1f4fe9aeae7ee89d5ba0
gharbat <arbioun@gmail.com>
2023-11-25 21:54:03 +0100

    Better urls

--- dashboard/data/conversations.ts
+++ dashboard/data/conversations.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8888/api/chat",
+  baseURL: "http://localhost:8888/backend/chat",
 });
 export type ChatMessageType = {
   id: string;
--- dashboard/data/knowledge.ts
+++ dashboard/data/knowledge.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8888/api",
+  baseURL: "http://localhost:8888/backend",
 });
 
 export async function uploadFile(file: File) {
