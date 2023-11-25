866fb49be317ea5a6a88fc933ae03b37ee63a0f6
ah7255703 <ah7255703@gmail.com>
2023-11-26 01:57:46 +0200

    Fix invalid class name in alert dialog, input, and textarea components

--- dashboard/components/ui/input.tsx
+++ dashboard/components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           "h-10 w-full m-0 bg-white shadow-sm min-w-fit rounded-md border block focus:outline-none border-border bg-background px-3 py-2 text-sm ring-offset-accent file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-primary transition-colors disabled:cursor-not-allowed disabled:opacity-50",
-          "data-[valid=false]:!border-destructive",
+          "data-[invalid='true']:!border-destructive",
           className
         )}
         ref={ref}
