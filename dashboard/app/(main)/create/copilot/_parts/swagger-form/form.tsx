967eab443de8d8b44e0d251301fb255fb1ec7e7d
ah7255703 <ah7255703@gmail.com>
2023-11-26 01:59:02 +0200

    Add test functionality to Swagger form

--- dashboard/app/(main)/create/copilot/_parts/swagger-form/form.tsx
+++ dashboard/app/(main)/create/copilot/_parts/swagger-form/form.tsx
@@ -17,6 +17,9 @@ import { Play, Plus, X } from "lucide-react";
 import _ from "lodash";
 import { FormValues, FormValuesWithId, methods, swaggerFormSchema } from "./types";
 import { useCreateCopilot } from "../CreateCopilotProvider";
+import { toast } from "@/components/ui/use-toast";
+import { atom, useAtom } from "jotai";
+import axios, { AxiosResponse } from "axios";
 
 interface FormSelectProps extends UseFormRegisterReturn {
     children?: ReactNode;
@@ -38,13 +41,21 @@ const FormSelect = forwardRef<HTMLButtonElement, FormSelectProps>(({ name, onCha
 
 FormSelect.displayName = "FormSelect";
 
-export function SwaggerForm({ defaultValues }: { defaultValues: FormValuesWithId }) {
+const testAtom = atom<{
+    endpointId: string;
+    response: AxiosResponse<any>;
+    isOk: boolean;
+}[]>([])
+
+const isTestingAtom = atom(false);
+
+export function SwaggerForm({ defaultValues }: { defaultValues?: FormValuesWithId }) {
     const { state, dispatch } = useCreateCopilot();
     const currentlyEditingEndpointId = state.currentlyEditingEndpointId;
-    const form = useForm<FormValues>({
-        mode: "onBlur",
+    const { register, handleSubmit, ...form } = useForm<FormValues>({
         resolver: zodResolver(swaggerFormSchema),
-        defaultValues,
+        defaultValues: defaultValues,
+        mode: "onChange"
     });
     const { fields: headerFields, append: appendHeaderField, remove: removeHeaderField } = useFieldArray({
         name: "headers",
@@ -55,42 +66,103 @@ export function SwaggerForm({ defaultValues }: { defaultValues: FormValuesWithId
         name: "parameters",
         control: form.control,
     })
-
+    const [testResults, setTestResults] = useAtom(testAtom);
+    const [testing, setTesting] = useAtom(isTestingAtom);
+    
     async function onsubmitHandler(values: FormValues) {
-        if (currentlyEditingEndpointId) {
-            // not save but update
-            const index = state.swaggerEndpoints.findIndex((endpoint) => endpoint.id === currentlyEditingEndpointId);
+        const mode = state.mode;
+        if (mode === 'CREATE_NEW_ENDPOINT') {
+            console.log("Creaet new endpoint", values)
+            dispatch({
+                type: "ADD_NEW_ENDPOINT",
+                payload: { ...values, id: _.uniqueId() }
+            })
+            toast({
+                title: "Endpoint added",
+                description: "Endpoint has been added to the list.",
+                variant: "success"
+            })
+            form.reset()
+            dispatch({
+                type: "CHANGE_MODE",
+                payload: {
+                    mode: undefined,
+                    endpointId: undefined
+                }
+            })
+        } else if (mode === 'EDIT_EXISTING_ENDPOINT' && defaultValues) {
+            console.log("Edit existing endpoint", defaultValues.id)
+            const index = state.swaggerEndpoints.findIndex((endpoint) => endpoint.id === defaultValues.id);
             const newEndpoints = _.cloneDeep(state.swaggerEndpoints);
-            newEndpoints[index] = { ...values, id: currentlyEditingEndpointId };
+            newEndpoints[index] = { ...values, id: defaultValues.id };
             dispatch({
                 type: "SET_SWAGGER_ENDPOINTS",
                 payload: newEndpoints
             })
+            toast({
+                title: "Endpoint updated",
+                description: `Endpoint ${values.title} has been updated.`,
+                variant: "success"
+            })
         }
-        dispatch({
-            type: "SET_CURRENTLY_EDITING_ENDPOINT_ID",
-            payload: null
-        })
-    }
 
+    }
+    
+    async function testEndpoint() {
+        try {
+            if (!form.formState.isValid) {
+                toast({
+                    title: "Invalid form",
+                    description: "Please fix the errors in the form.",
+                    variant: "destructive"
+                })
+                return;
+            } else if (!currentlyEditingEndpointId) {
+                toast({
+                    title: "You should save the endpoint first",
+                    description: "You should save the endpoint first",
+                    variant: "destructive"
+                })
+            } else {
+                setTesting(true);
+                const request = await axios.request({
+                    method: form.watch('method'),
+                    url: form.watch('url'),
+                    data: form.watch('body'),
+                    headers: _.fromPairs(headerFields.map((field) => [field.key, field.value])),
+                    params: _.fromPairs(parameterFields.map((field) => [field.key, field.value])),
+                })
+                setTestResults((prev) => [...prev, {
+                    endpointId: currentlyEditingEndpointId,
+                    response: request,
+                    isOk: request.status >= 200 && request.status < 300
+                }])
+                setTesting(false);
+            }
+        } catch (error) {
+            setTesting(true);
+        }
+
+    }
+    
     return (
-        <AlertDialog open={!!currentlyEditingEndpointId}>
-            <AlertDialogContent asChild>
-                <form onSubmit={form.handleSubmit(onsubmitHandler)}>
+        <AlertDialog open={!!state.mode}>
+            <AlertDialogContent asChild className="overflow-auto">
+                <form onSubmit={handleSubmit(onsubmitHandler)}>
                     <AlertDialogHeader className="flex items-center justify-between w-full flex-row">
                         <AlertDialogTitle className="flex-1">
                             API
                         </AlertDialogTitle>
-                        <Button variant="success" size="sm" type="button">
+                        <Button variant="success" size="sm" type="button" onClick={testEndpoint} loading={testing} disabled={testing}>
                             <Play className="w-5 h-5" />
                             Test
                         </Button>
                     </AlertDialogHeader>
-                    <div className="w-full space-y-4">
-                        <Input placeholder="Title" {...form.register('title')} data-valid={!form.getFieldState('title').invalid} />
+                    <div className="w-full space-y-4 flex-1">
+                        <Input placeholder="Title" data-invalid={form.getFieldState('title').invalid}  {...register('title')} />
                         <div className="w-full">
-                            <div className="flex items-center h-12 gap-0.5 p-1 focus-within:!border-primary overflow-hidden border border-border w-full m-0 bg-white shadow-sm rounded-md focus:outline-none text-sm focus-visible:outline-none focus-visible:border-primary transition-colors">
-                                <FormSelect value={form.watch('method')} {...form.register('method')}>
+                            <div data-invalid={form.getFieldState('url').invalid || form.getFieldState('method').invalid} className="flex items-center data-[invalid='true']:!border-destructive h-12 gap-0.5 p-1 overflow-hidden border border-border w-full m-0 bg-white shadow-sm rounded-md focus:outline-none text-sm focus-visible:outline-none transition-colors">
+                                <FormSelect value={form.watch('method')} {...register('method')}>
                                     <SelectTrigger className="ring-0 w-fit h-full p-1.5 border-0 text-xs font-semibold">
                                         <SelectValue placeholder="Method" />
                                     </SelectTrigger>
@@ -104,13 +176,18 @@ export function SwaggerForm({ defaultValues }: { defaultValues: FormValuesWithId
                                         }
                                     </SelectContent>
                                 </FormSelect>
-                                <Input className="flex-1 border-0 h-full py-1.5 shadow-none" {...form.register('url')} placeholder="API endpoint" />
+                                <Input className="flex-1 border-0 h-full py-1.5 shadow-none" {...register('url')} placeholder="API endpoint" />
                             </div>
                             <p className="text-xs text-destructive mt-1">{form.getFieldState('url').error?.message}</p>
                         </div>
 
                         <div>
-                            <Textarea {...form.register('summary')} placeholder="summary" minRows={3} />
+                            <div className="w-full relative">
+                                <Textarea {...register('summary')} maxRows={5} data-invalid={form.getFieldState('summary').invalid} placeholder="Summary" minRows={2} />
+                                <span className="absolute -top-2 -right-2 bg-white px-1.5 py-0.5 text-xs rounded-md text-muted-foreground">
+                                    {form.watch('summary')?.length || 0}/50
+                                </span>
+                            </div>
                             <p className="text-xs text-muted-foreground mt-0.5">
                                 Consider adding a description to help the copilot understand the purpose of this API.
                             </p>
@@ -135,12 +212,12 @@ export function SwaggerForm({ defaultValues }: { defaultValues: FormValuesWithId
                                         const isValid = errorMessage === undefined;
                                         return (<div className="flex items-center gap-2 animate-in fade-in slide-in-from-top-1" key={field.id}>
                                             <Input placeholder="Key"
-                                                {...form.register(`headers.${index}.key`)}
+                                                {...register(`headers.${index}.key`)}
                                                 data-valid={isValid}
                                             />
                                             <Input placeholder="Value"
                                                 data-valid={isValid}
-                                                {...form.register(`headers.${index}.value`)}
+                                                {...register(`headers.${index}.value`)}
                                             />
                                             <button className="shrink-0 p-2 text-destructive" type="button"
                                                 onClick={() => removeHeaderField(index)}>
@@ -170,12 +247,12 @@ export function SwaggerForm({ defaultValues }: { defaultValues: FormValuesWithId
                                         const isValid = errorMessage === undefined;
                                         return (<div className="flex items-center gap-2 animate-in fade-in slide-in-from-top-1" key={field.id}>
                                             <Input placeholder="Key"
-                                                {...form.register(`parameters.${index}.key`)}
+                                                {...register(`parameters.${index}.key`)}
                                                 data-valid={isValid}
                                             />
                                             <Input placeholder="Value"
                                                 data-valid={isValid}
-                                                {...form.register(`parameters.${index}.value`)}
+                                                {...register(`parameters.${index}.value`)}
                                             />
                                             <button className="shrink-0 p-2 text-destructive" type="button"
                                                 onClick={() => removeParameterField(index)}>
@@ -186,11 +263,19 @@ export function SwaggerForm({ defaultValues }: { defaultValues: FormValuesWithId
                                 }
                             </div>
                         </div>
-
-                        <AlertDialogFooter className="gap-2">
+                        <div className="space-y-2">
+                            <Label htmlFor="request-body" >
+                                Body
+                            </Label>
+                            <Textarea id="request-body" {...register('body')} placeholder="Request Body" minRows={4} />
+                        </div>
+                        <AlertDialogFooter className="gap-2 sticky bottom-0 left-0 bg-white w-full">
                             <AlertDialogCancel onClick={() => dispatch({
-                                type: "SET_CURRENTLY_EDITING_ENDPOINT_ID",
-                                payload: null
+                                type: "CHANGE_MODE",
+                                payload: {
+                                    mode: undefined,
+                                    endpointId: undefined
+                                }
                             })}>
                                 Cancel
                             </AlertDialogCancel>
--- dashboard/app/(main)/create/copilot/_parts/swagger-form/index.tsx
+++ dashboard/app/(main)/create/copilot/_parts/swagger-form/index.tsx
@@ -1,5 +1,4 @@
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { MoreVertical, Plus } from "lucide-react";
+import { Pen, Plus, Trash2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import _ from "lodash";
 import { Label } from "@/components/ui/label";
@@ -10,13 +9,15 @@ import { useCreateCopilot } from "../CreateCopilotProvider";
 
 export function SwaggerUi() {
     const { state, dispatch } = useCreateCopilot();
+    const swaggerUrls = state.swaggerEndpoints;
     const currentlyEditingEndpointId = state.currentlyEditingEndpointId;
     const currentlyEditingEndpoint = state.swaggerEndpoints.find((endpoint) => endpoint.id === currentlyEditingEndpointId);
-    const swaggerUrls = state.swaggerEndpoints;
     function addNewEndpoint() {
-        setCurrentlyEditingEndpoint(null);
         dispatch({
-            type: "ADD_NEW_ENDPOINT"
+            type: "CHANGE_MODE",
+            payload: {
+                mode: "CREATE_NEW_ENDPOINT",
+            }
         })
     }
     function handleDelete(id: string) {
@@ -25,24 +26,18 @@ export function SwaggerUi() {
             payload: id
         })
     }
-    function setCurrentlyEditingEndpoint(id: string | null) {
-        dispatch({
-            type: "SET_CURRENTLY_EDITING_ENDPOINT_ID",
-            payload: id
-        })
-    }
 
     return (
-        <>
+        <div>
             <div className="flex items-center justify-between">
                 <Label className="my-4 text-sm text-accent-foreground">
-                    Manually Create Swagger Def.
+                    Use our straight forward form to add new endpoints.
                 </Label>
                 <button onClick={addNewEndpoint}>
                     <Plus className="w-5 h-5" />
                 </button>
             </div>
-            <div className="flex items-start flex-col gap-2 overflow-auto max-h-52 p-1.5">
+            <div className="flex items-start flex-col gap-2 overflow-auto max-h-52 p-0.5">
                 {
                     swaggerUrls.length === 0 ?
                         <div className="mx-auto">
@@ -52,7 +47,7 @@ export function SwaggerUi() {
                         </div>
                         :
                         swaggerUrls.map((endpoint, index) => {
-                            return <div key={index} className="w-full p-2 shrink-0 flex overflow-hidden max-w-full items-center justify-between border border-border transition-colors rounded-lg">
+                            return <div key={index} className="w-full p-1.5 shrink-0 flex overflow-hidden max-w-full gap-4 items-center justify-between border border-border transition-colors rounded-lg">
                                 <div className="flex-1 flex items-center justify-start overflow-hidden shrink-0">
                                     <div className="flex items-center gap-5 overflow-hidden shrink-0">
                                         <span className={cn(methodVariants({
@@ -60,32 +55,34 @@ export function SwaggerUi() {
                                         }))}>
                                             {endpoint.method}
                                         </span>
-                                        <p className="flex-1 line-clamp-1 overflow-ellipsis text-sm">
+                                        <p className="flex-1 line-clamp-1 overflow-ellipsis font-medium text-xs">
                                             {endpoint.url || endpoint.title}
                                         </p>
                                     </div>
                                 </div>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger>
-                                        <MoreVertical className="w-5 h-5" />
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent align="end">
-                                        <DropdownMenuItem className="text-destructive" onClick={() => handleDelete(endpoint.id)}>
-                                            Delete
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem className="text-accent-foreground" onClick={() => setCurrentlyEditingEndpoint(endpoint.id)}>
-                                            Edit
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
+                                <div className="space-x-2">
+                                    <button className="text-primary" onClick={() => {
+                                        dispatch({
+                                            type: "CHANGE_MODE",
+                                            payload: {
+                                                mode: "EDIT_EXISTING_ENDPOINT",
+                                                endpointId: endpoint.id
+                                            }
+                                        })
+                                    }}>
+                                        <Pen className="w-4 h-4" />
+                                    </button>
+                                    <button className="text-destructive">
+                                        <Trash2 className="w-4 h-4" onClick={() => confirm("are you sure") && handleDelete(endpoint.id)} />
+                                    </button>
+                                </div>
                             </div>
                         })
                 }
             </div>
-            {
-                currentlyEditingEndpoint && <SwaggerForm defaultValues={currentlyEditingEndpoint} />
-            }
-        </>
+            {state.mode && <SwaggerForm defaultValues={currentlyEditingEndpoint} />}
+        </div>
+
     )
 
 }
