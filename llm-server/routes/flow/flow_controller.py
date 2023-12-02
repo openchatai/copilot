b6993068475da95bd6dfd2011c17a7e2982dc319
gharbat <arbioun@gmail.com>
2023-12-02 09:07:12 +0100

    Update skeleton responses

--- llm-server/routes/flow/flow_controller.py
+++ llm-server/routes/flow/flow_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, jsonify
 
 from utils.db import Database
 
@@ -10,17 +10,70 @@ flow = Blueprint("flow", __name__)
 
 @flow.route("/bot/<bot_id>/flows", methods=["GET"])
 def get_all_flows_by_bot_id(bot_id: str) -> Response:
-    pass
+    return jsonify([
+        {
+            'id': 'uuid4',
+            'name': 'Add to cart flow',
+        },
+        {
+            'id': 'uuid4',
+            'name': 'Add to cart flow',
+        },
+        {
+            'id': 'uuid4',
+            'name': 'Add to cart flow',
+        },
+        {
+            'id': 'uuid4',
+            'name': 'Add to cart flow',
+        }
+    ])
 
 
 @flow.route("/<flow_id>", methods=["GET"])
 def get_flow_by_id(flow_id: str) -> Response:
-    pass
+    return jsonify({
+        'id': 'uuid4',
+        'name': 'Add to cart flow',
+        'blocks': [
+            {
+                'id': 'action_id',
+                'order': 1,
+                'type': 'block',
+                'next_on_success': 'some_action_id',
+                'next_on_fail': 'some_action_id',
+                'name': 'Create stuff',
+                'apis': [
+                    {
+                        # ... Swagger endpoint
+                        'parameters': {
+                            'x': '{env_x}',
+                            'y': 'magic_y'
+                        }
+                    },
+
+                ]
+
+            },
+            # ...
+        ]
+    })
 
 
 @flow.route("/<flow_id>/variables", methods=["GET"])
 def get_flow_variables(flow_id: str) -> Response:
-    pass
+    return jsonify({
+        'flow_id': "uuid4",
+        'name': 'Flow name',
+        'variables': [
+            {
+                'name': '{xxx}',
+                'dynamic_value_from': 'action_id.json.key',
+                'static_value': '',
+                'is_magic_fill': False
+            }
+        ]
+    })
 
 
 @flow.route("/<flow_id>/variables", methods=["POST", "PUT"])
