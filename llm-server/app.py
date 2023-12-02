c24c22c7a00a42e3c0cf5322594c111f035fcb3f
gharbat <arbioun@gmail.com>
2023-12-02 13:35:38 +0100

    Add the route to app.py

--- llm-server/app.py
+++ llm-server/app.py
@@ -1,17 +1,18 @@
 from dotenv import load_dotenv
 from flask import Flask
+from opencopilot_db import create_database_schema
 
 from routes._swagger.controller import _swagger
 from routes.chat.chat_controller import chat_workflow
 from routes.copilot.copilot_controller import copilot
 from routes.data_source.data_source_controller import datasource_workflow
-from routes.prompt.prompt_template_controller import prompt_template_workflow
+from routes.flow.flow_controller import flow
 from routes.prompt.prompt_controller import prompt_workflow
+from routes.prompt.prompt_template_controller import prompt_template_workflow
 from routes.uploads.upload_controller import upload_controller
 from routes.workflow.workflow_controller import workflow
 from utils.config import Config
 from utils.vector_store_setup import init_qdrant_collections
-from opencopilot_db import create_database_schema
 
 load_dotenv()
 
@@ -27,6 +28,7 @@ app.register_blueprint(upload_controller, url_prefix="/backend/uploads")
 app.register_blueprint(datasource_workflow, url_prefix="/backend/data_sources")
 app.register_blueprint(prompt_template_workflow, url_prefix="/backend/prompt-templates")
 app.register_blueprint(prompt_workflow, url_prefix="/backend/prompts")
+app.register_blueprint(flow, url_prefix="/backend/flows-v2")  ## todo rename
 
 app.config.from_object(Config)
 
