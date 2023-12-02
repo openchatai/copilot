0c6997262747e057fb41ae8c5910dfefa6fabec4
gharbat <arbioun@gmail.com>
2023-12-02 09:49:09 +0100

    Add flows table

---
+++ workers/shared/models/opencopilot_db/flow.py
@@ -0,0 +1,19 @@
+import uuid
+
+from opencopilot_db.database_setup import Base, engine
+from sqlalchemy import Column, String, DateTime, Boolean, Text
+import datetime
+
+
+class Chatbot(Base):
+    __tablename__ = 'flows'
+
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = Column(String(255))
+    status = Column(String(255), default='draft')
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    deleted_at = Column(DateTime, nullable=True)
+
+
+Base.metadata.create_all(engine)
