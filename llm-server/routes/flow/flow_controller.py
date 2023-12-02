48c7ee574a0e0bd636caa98f59202d0452a604cb
gharbat <arbioun@gmail.com>
2023-12-02 08:40:19 +0100

    Update the method types

--- llm-server/routes/flow/flow_controller.py
+++ llm-server/routes/flow/flow_controller.py
@@ -8,41 +8,36 @@ mongo = db_instance.get_db()
 flow = Blueprint("flow", __name__)
 
 
-@flow.route("/", methods=["GET"])
-def get_all_flows_by_bot_id(session_id: str) -> Response:
+@flow.route("/bot/<bot_id>/flows", methods=["GET"])
+def get_all_flows_by_bot_id(bot_id: str) -> Response:
     pass
 
 
-@flow.route("/", methods=["GET"])
-def get_flow_by_id(session_id: str) -> Response:
+@flow.route("/<flow_id>", methods=["GET"])
+def get_flow_by_id(flow_id: str) -> Response:
     pass
 
 
-@flow.route("/", methods=["GET"])
-def get_flow_variables(session_id: str) -> Response:
+@flow.route("/<flow_id>/variables", methods=["GET"])
+def get_flow_variables(flow_id: str) -> Response:
     pass
 
 
-@flow.route("/", methods=["GET"])
-def add_variables_to_flow(session_id: str) -> Response:
+@flow.route("/<flow_id>/variables", methods=["POST", "PUT"])
+def add_variables_to_flow(flow_id: str) -> Response:
     pass
 
 
-@flow.route("/", methods=["GET"])
-def add_action_to_flow(session_id: str) -> Response:
+@flow.route("/<flow_id>/actions", methods=["POST"])
+def add_action_to_flow(flow_id: str) -> Response:
     pass
 
 
-@flow.route("/", methods=["GET"])
-def remove_action_from_flow(session_id: str) -> Response:
+@flow.route("/<flow_id>/actions", methods=["DELETE"])
+def remove_action_from_flow(flow_id: str) -> Response:
     pass
 
 
-@flow.route("/", methods=["GET"])
-def add_action_to_flow_from_defined_one(session_id: str) -> Response:
-    pass
-
-
-@flow.route("/", methods=["GET"])
+@flow.route("/<flow_id>/actions", methods=["PATCH"])
 def update_action_in_flow(session_id: str) -> Response:
     pass
