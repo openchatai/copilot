09fa6b9b5b853a41a08dfb9e4bb8ed65f510dff8
gharbat <arbioun@gmail.com>
2023-12-02 09:54:01 +0100

    Add flow blocks table

--- workers/shared/models/opencopilot_db/flow_block.py
+++ workers/shared/models/opencopilot_db/flow_block.py
@@ -2,7 +2,7 @@ import datetime
 import uuid
 
 from opencopilot_db.database_setup import Base, engine
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, Integer
 
 
 class Chatbot(Base):
@@ -13,6 +13,10 @@ class Chatbot(Base):
     chatbot_id = Column(String(36), nullable=True)
     flow_id = Column(String(36), nullable=True)
     status = Column(String(255), default='draft')
+    type = Column(String(255), default='block')
+    next_on_success = Column(String(255), default=None)
+    next_on_fail = Column(String(255), default=None)
+    order = Column(Integer)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
     deleted_at = Column(DateTime, nullable=True)
