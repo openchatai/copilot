0a303d243860a42a881a08100e3d39c7489c33c1
gharbat <arbioun@gmail.com>
2023-12-03 21:08:08 +0100

    Nested varS

--- llm-server/presenters/flow_presenters.py
+++ llm-server/presenters/flow_presenters.py
@@ -7,14 +7,28 @@ Session = sessionmaker(bind=engine)
 
 
 def flow_to_dict(flow: Flow):
-    """Convert a Flow object to a dictionary."""
-    return {
-        "flow_id": flow.id.hex() if isinstance(flow.id, bytes) else flow.id,
-        "name": flow.name,
-        "payload": flow.payload,
-        "description": flow.description,
-        "last_saved_at": flow.updated_at.isoformat() if flow.updated_at else None,
-    }
+    """
+    Convert a Flow object to a dictionary, including its associated variables.
+
+    Args:
+        flow: The Flow object.
+
+    Returns:
+        A dictionary representation of the Flow, including its variables.
+    """
+    with Session() as session:
+        # Query for variables associated with the flow
+        variables = session.query(FlowVariable).filter(FlowVariable.flow_id == flow.id).all()
+        variables_dict = [flow_variable_to_dict(variable) for variable in variables]
+
+        return {
+            "flow_id": flow.id.hex() if isinstance(flow.id, bytes) else flow.id,
+            "name": flow.name,
+            "payload": flow.payload,
+            "description": flow.description,
+            "last_saved_at": flow.updated_at.isoformat() if flow.updated_at else None,
+            "variables": variables_dict  # Including nested variables
+        }
 
 
 def flow_to_simplified_dict(flow: Flow):
