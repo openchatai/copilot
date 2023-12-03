06ca3308bb5b305913ac28c4c7d31fffa7b7bfae
ah7255703 <ah7255703@gmail.com>
2023-12-03 18:28:29 +0200

    Refactor workflows list and endpoint node components

--- dashboard/app/(copilot)/copilot/[copilot_id]/flows/_parts/WorkflowsList.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/flows/_parts/WorkflowsList.tsx
@@ -1,36 +1,19 @@
 "use client";
+import { NavLink } from "@/components/ui/NavLink";
 import _ from "lodash";
-import React from "react";
-import { useController } from "@/components/domain/flows-editor";
-import { cn } from "@/lib/utils";
-import useSWR from "swr";
-import { getWorkflowsByBotId } from "@/data/flow";
 
 export function WorkflowsList({ copilot_id }: { copilot_id: string }) {
   const flowsBase = `/copilot/${copilot_id}/flows`;
-  const {
-    state: { flows, activeFlowId },
-    setActiveFlow,
-    loadData
-  } = useController();
-  const { isLoading } = useSWR('flows/' + copilot_id, () => getWorkflowsByBotId(copilot_id), {
-    onSuccess: ({ data }) => {
-      const firstFlow = _.first(data.workflows)
-      if (firstFlow) {
-        loadData(firstFlow)
-      }
-    }
-  })
-  return <ul className="space-y-1 px-2">
-    {
-      flows.map((flow, i) => {
-        const isActive = flow.id === activeFlowId
-        return <li key={i}>
-          <button onClick={() => setActiveFlow(flow.id)} className={cn("w-full font-medium flex items-center rounded border px-4 py-2 text-sm text-accent-foreground transition duration-150 ease-in-out", isActive && "border-gray-300 bg-accent border-primary font-semibold ")}>
-            {flow.name}
-          </button>
-        </li>
-      })
-    }
+  return <ul className="px-2 space-y-2">
+    <li>
+      <NavLink
+        href={flowsBase + "/?workflow_id=" + "first_flow"}
+        matchSearchParams
+        className="flex items-center rounded border px-4 py-2 text-sm font-medium text-accent-foreground transition duration-150 ease-in-out "
+        activeClassName="border-gray-300 bg-accent"
+      >
+        First flow
+      </NavLink>
+    </li>
   </ul>
 }
