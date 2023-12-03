740d4e3774a139af664f66ba5c43a385a7a71f44
shanurrahman <shanur.cse.nitap@gmail.com>
2023-12-03 03:53:08 +0300

    handling some errors

--- llm-server/routes/_swagger/reindex_service.py
+++ llm-server/routes/_swagger/reindex_service.py
@@ -1,4 +1,6 @@
 import os
+from urllib.parse import urlparse
+from jsonschema import RefResolutionError, ValidationError
 from qdrant_client import QdrantClient
 from routes._swagger.service import save_swagger_paths_to_qdrant
 from utils.db import Database
@@ -25,9 +27,38 @@ def process_swagger_files_batch(swagger_files: Iterable[Any]):
 
 
 def process_swagger_file(swagger_file: Any):
-    bot_id: str = swagger_file["meta"]["bot_id"]
-    swagger_url = swagger_file["meta"]["swagger_url"]
+    try:
+        bot_id: str = swagger_file["meta"]["bot_id"]
+        swagger_url = swagger_file["meta"]["swagger_url"]
 
-    swagger_doc = ResolvingParser(url=swagger_url)
+        # Check if swagger_url is a valid URL
+        if not is_valid_url(swagger_url):
+            swagger_url = "/app/shared_data/url"
 
-    save_swagger_paths_to_qdrant(swagger_doc=swagger_doc, bot_id=bot_id)
+        swagger_doc = ResolvingParser(url=swagger_url)
+
+        save_swagger_paths_to_qdrant(swagger_doc=swagger_doc, bot_id=bot_id)
+
+    except KeyError as e:
+        print(f"Error: Missing key in swagger_file - {e}")
+        # Handle the missing key error here
+
+    except RefResolutionError as e:
+        print(f"Error resolving JSON references - {e}")
+        # Handle reference resolution error here
+
+    except ValidationError as e:
+        print(f"Error in JSON schema validation - {e}")
+        # Handle JSON schema validation error here
+
+    except Exception as e:
+        print(f"An unexpected error occurred - {e}")
+        # Handle other unexpected errors here
+
+
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
