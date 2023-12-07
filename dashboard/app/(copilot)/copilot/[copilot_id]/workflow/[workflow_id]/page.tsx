96aabac71d63d50d7917184783218d9e14f32fb2
ah7255703 <ah7255703@gmail.com>
2023-12-07 21:20:26 +0200

    Add workflow page with flow renderer

---
+++ dashboard/app/(copilot)/copilot/[copilot_id]/workflow/[workflow_id]/page.tsx
@@ -0,0 +1,9 @@
+import { FlowRenderer } from '@/components/domain/new-flows-editor/Renderer'
+import React from 'react'
+
+
+export default function WorkflowPage() {
+    return (
+        <FlowRenderer />
+    )
+}
