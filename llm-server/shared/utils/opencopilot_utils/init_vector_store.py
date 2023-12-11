d03e2153f4a8a4c80bf179a71a37657232d73b3b
gharbat <arbioun@gmail.com>
2023-12-11 11:47:41 +0100

    Add flow vector service

--- llm-server/shared/utils/opencopilot_utils/init_vector_store.py
+++ llm-server/shared/utils/opencopilot_utils/init_vector_store.py
@@ -1,18 +1,22 @@
-from langchain.docstore.document import Document
-from langchain.vectorstores.qdrant import Qdrant
-from .store_type import StoreType
-from langchain.embeddings.base import Embeddings
-from .interfaces import StoreOptions
 import os
+
+from langchain.docstore.document import Document
+from langchain.embeddings.base import Embeddings
+from langchain.vectorstores.qdrant import Qdrant
+
+from .interfaces import StoreOptions
+from .store_type import StoreType
+
 
 def init_vector_store(docs: list[Document], embeddings: Embeddings, options: StoreOptions) -> None:
     store_type = StoreType[os.environ['STORE']]
-    
+
     for doc in docs:
         doc.metadata.update(options.metadata)
-        
+
     if store_type == StoreType.QDRANT:
         Qdrant.from_documents(docs, embeddings, collection_name=options.namespace, url=os.environ['QDRANT_URL'])
     else:
         valid_stores = ", ".join(StoreType._member_names())
-        raise ValueError(f"Invalid STORE environment variable value: {os.environ['STORE']}. Valid values are: {valid_stores}")
+        raise ValueError(
+            f"Invalid STORE environment variable value: {os.environ['STORE']}. Valid values are: {valid_stores}")
