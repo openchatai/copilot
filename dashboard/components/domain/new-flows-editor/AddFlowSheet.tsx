88500a6b4a48026094205afd53e84e5e6aaa7b38
ah7255703 <ah7255703@gmail.com>
2023-12-12 00:36:55 +0200

    Handle On submit-create new action-

--- dashboard/components/domain/new-flows-editor/AddFlowSheet.tsx
+++ dashboard/components/domain/new-flows-editor/AddFlowSheet.tsx
@@ -1,15 +1,32 @@
 'use client'
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetFooter } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { atom, useAtom } from "jotai";
-import { ActionForm } from "./ActionForm";
+import { ActionForm } from "../action-form/ActionForm";
+import { ActionType } from "../action-form/schema";
+import { useHotkeys } from "react-hotkeys-hook";
+import { createActionByBotId as createActionPromiseByBotId } from "@/data/actions";
+import { useCopilot } from "@/app/(copilot)/copilot/_context/CopilotProvider";
+import { useAsyncFn } from 'react-use';
+import { revalidateActions } from "./Controller";
 
-const drawerStateAtom = atom(false);
-export const useDrawerState = () => useAtom(drawerStateAtom);
+const addActionFormState = atom(false);
+export const useActionFormState = () => useAtom(addActionFormState);
 
 
 export function AddActionDrawer() {
-    const [drawerState, setDrawerState] = useDrawerState();
+    const [drawerState, setDrawerState] = useActionFormState();
+    const { id: copilotId } = useCopilot();
+    const [state, createActionByBotId] = useAsyncFn(createActionPromiseByBotId);
+    async function handleOnSubmit(values: ActionType) {
+        const { data } = await createActionByBotId(copilotId, values);
+        if (data.id) {
+            revalidateActions(copilotId);
+        }
+    }
+    useHotkeys("ctrl+/", () => {
+        setDrawerState(true);
+    })
 
     return (
         <Sheet open={drawerState} onOpenChange={setDrawerState}>
@@ -23,13 +40,15 @@ export function AddActionDrawer() {
                     </SheetDescription>
                 </SheetHeader>
                 <div className="w-full flex-1">
-                    <ActionForm />
+                    <ActionForm onSubmit={handleOnSubmit}
+                        className="h-full flex flex-col justify-between"
+                        footer={
+                            (form) => (<div className="space-x-2 mt-auto">
+                                <Button disabled={!form.formState.isValid} loading={state.loading} type="submit">Create</Button>
+                                <Button variant='ghost' onClick={() => setDrawerState(false)}>Cancel</Button>
+                            </div>)
+                        } />
                 </div>
-                
-                <SheetFooter>
-                    <Button>Create</Button>
-                    <Button variant='ghost' onClick={() => setDrawerState(false)}>Cancel</Button>
-                </SheetFooter>
             </SheetContent>
         </Sheet>
 
