5565fc44e5dceb76022030bedd5b15630e437c79
ah7255703 <ah7255703@gmail.com>
2023-12-13 22:14:56 +0200

    Add xs size variant to button

--- dashboard/components/ui/button.tsx
+++ dashboard/components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
+        xs: "h-7 rounded-md px-2 text-xs",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
         fluid: "w-full h-10",
