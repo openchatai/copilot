e4d0c564c08d390c7d714cb1631040b1a92872a7
ah7255703 <ah7255703@gmail.com>
2023-12-13 22:24:37 +0200

    Remove unused import and fix import path

--- dashboard/components/domain/new-flows-editor/types/block.ts
+++ dashboard/components/domain/new-flows-editor/types/block.ts
@@ -1,10 +1,11 @@
 import { Node } from "reactflow";
-import { CombineTypes, SharedFields } from "./utils";
+import { SharedFields } from "./utils";
+import { CombineTypes } from "@/types/utils";
 
 export type BlockType = CombineTypes<[{
     name: string;
     description: string;
-    next_on_success:string // just a block id 
+    next_on_success: string // just a block id 
 }, SharedFields]>
 
 export type BlockNodeType = Node<BlockType>;
