36c969d17ba56649ff778a040bfc0855be78f152
ah7255703 <ah7255703@gmail.com>
2023-12-13 23:04:13 +0200

    Delete unused Swagger form files

--- dashboard/app/(main)/create/copilot/_parts/swagger-form/types.ts
+++
@@ -1,5 +0,0 @@
-import zod from 'zod';
-import { actionSchema } from "@/components/domain/action-form/schema";
-
-export type FormValuesWithId = FormValues & { id: string };
-export type FormValues = zod.infer<typeof actionSchema>
