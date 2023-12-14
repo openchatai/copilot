39250b0790d99230ea18758a8ae4cee80dd7d537
ah7255703 <ah7255703@gmail.com>
2023-12-14 21:57:22 +0200

    Add block manipulation functions to Controller

--- dashboard/components/domain/new-flows-editor/Controller.tsx
+++ dashboard/components/domain/new-flows-editor/Controller.tsx
@@ -6,12 +6,23 @@ import { Union } from '@/types/utils';
 import { ActionResponseType, getActionsByBotId } from '@/data/actions';
 import { useCopilot } from '@/app/(copilot)/copilot/_context/CopilotProvider';
 import useSWR, { mutate } from 'swr';
+import { BlockType } from './types/block';
+import _ from 'lodash';
+
 
 type ControllerType = {
     state: StateType,
     // eslint-disable-next-line no-unused-vars
     loadActions: (actions: ActionResponseType[]) => void,
     reset: () => void,
+    // eslint-disable-next-line no-unused-vars
+    updateBlock: (blockId: string, block: Partial<BlockType>) => void,
+    // eslint-disable-next-line no-unused-vars
+    reorderActions: (sourceIndex: number, destinationIndex: number) => void,
+    // eslint-disable-next-line no-unused-vars
+    addActionToBlock: (blockId: string, action: ActionResponseType, index: number) => void,
+    // eslint-disable-next-line no-unused-vars
+    reorderActionsInBlock: (blockId: string, sourceIndex: number, destinationIndex: number) => void,
 }
 const [ControllerProvider, useController] = createSafeContext<ControllerType>('Controller');
 
@@ -31,20 +42,70 @@ type ActionsType = Union<[{
     payload: {
         blockType: "ON_ERROR" | "ON_SUCCESS" | null
     }
+}, {
+    type: "UPDATE_BLOCK",
+    payload: {
+        blockId: string,
+        data: Partial<BlockType>
+    }
+}, {
+    type: "REORDER_ACTIONS",
+    payload: {
+        sourceIndex: number,
+        destinationIndex: number,
+    }
+}, {
+    type: "ADD_ACTION_TO_BLOCK",
+    payload: {
+        blockId: string,
+        action: ActionResponseType,
+        index: number,
+    }
+}, {
+    type: "REORDER_ACTIONS_IN_BLOCK",
+    payload: {
+        blockId: string,
+        sourceIndex: number,
+        destinationIndex: number,
+    }
 }]>;
 
 type StateType = {
     name: string | null,
     description: string | null,
     actions: ActionResponseType[],
-    blocks: any[],
+    blocks: BlockType[],
 }
 
 const initialState: StateType = {
     name: null,
     description: null,
     actions: [],
-    blocks: [],
+    blocks: [{
+        id: "block-1",
+        name: "Start",
+        created_at: new Date().toISOString(),
+        next_on_success: null,
+        updated_at: new Date().toISOString(),
+        actions: [{
+            id: "action-1",
+            name: "Create User",
+            description: "Create a new user",
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+            request_type: "POST",
+            api_endpoint: "https://api.example.com/users",
+            operation_id: "create_user",
+            deleted_at: null,
+        }],
+    }],
+}
+export function reorderList<T extends any>(list: T[], startIndex: number, endIndex: number) {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    if (!removed) return [...result];
+    result.splice(endIndex, 0, removed);
+    return [...result];
 }
 
 function stateReducer(state: StateType, action: ActionsType) {
@@ -55,6 +116,36 @@ function stateReducer(state: StateType, action: ActionsType) {
             case "LOAD_ACTIONS":
                 draft.actions = action.payload;
                 return;
+            case "UPDATE_BLOCK": {
+                // not all data will change, so we need to merge the data
+                const { blockId, data } = action.payload;
+                const block = draft.blocks.find(block => block.id === blockId);
+                if (block) {
+                    _.assign(block, data);
+                }
+                return;
+            }
+            case "ADD_ACTION_TO_BLOCK": {
+                const { blockId, action: $action, index } = action.payload;
+                const block = draft.blocks.find(block => block.id === blockId);
+                if (block) {
+                    block.actions.splice(index, 0, $action);
+                }
+                return;
+            }
+            case "REORDER_ACTIONS": {
+                const { sourceIndex, destinationIndex } = action.payload;
+                draft.actions = reorderList(draft.actions, sourceIndex, destinationIndex);
+                return;
+            }
+            case "REORDER_ACTIONS_IN_BLOCK": {
+                const { blockId, sourceIndex, destinationIndex } = action.payload;
+                const block = draft.blocks.find(block => block.id === blockId);
+                if (block) {
+                    block.actions = reorderList(block.actions, sourceIndex, destinationIndex);
+                }
+                return;
+            }
         }
     })
 }
@@ -62,6 +153,7 @@ function stateReducer(state: StateType, action: ActionsType) {
 function FlowsControllerV2({ children }: { children: React.ReactNode }) {
     const [state, dispatch] = useReducer(stateReducer, initialState);
     const { id: copilotId } = useCopilot();
+    console.log("copilotId", state)
     useSWR("actions/" + copilotId, async () => (await getActionsByBotId(copilotId)).data, {
         onSuccess: (data) => {
             dispatch({ type: "LOAD_ACTIONS", payload: data })
@@ -76,7 +168,23 @@ function FlowsControllerV2({ children }: { children: React.ReactNode }) {
         dispatch({ type: "RESET" })
     }, [])
 
-    return <ControllerProvider value={{ state, loadActions, reset }}>
+    const updateBlock = useCallback((blockId: string, block: Partial<BlockType>) => {
+        dispatch({ type: "UPDATE_BLOCK", payload: { blockId, data: block } })
+    }, [])
+
+    const reorderActions = useCallback((sourceIndex: number, destinationIndex: number) => {
+        dispatch({ type: "REORDER_ACTIONS", payload: { sourceIndex, destinationIndex } })
+    }
+        , [])
+    const addActionToBlock = useCallback((blockId: string, action: ActionResponseType, index: number) => {
+        dispatch({ type: "ADD_ACTION_TO_BLOCK", payload: { blockId, action, index } })
+    }
+        , [])
+    const reorderActionsInBlock = useCallback((blockId: string, sourceIndex: number, destinationIndex: number) => {
+        dispatch({ type: "REORDER_ACTIONS_IN_BLOCK", payload: { blockId, sourceIndex, destinationIndex } })
+    }
+        , [])
+    return <ControllerProvider value={{ state, loadActions, reset, updateBlock, reorderActions, addActionToBlock, reorderActionsInBlock }}>
         {children}
     </ControllerProvider>
 }
