2b7c32d6c0f897b3f0c2b4816815b9417918d13e
Shanur Rahman <shanur.cse.nitap@gmail.com>
2023-12-23 07:29:49 +0000

    fixed the function init vector store

--- llm-server/routes/uploads/celery_service.py
+++ llm-server/routes/uploads/celery_service.py
@@ -6,6 +6,6 @@ SHARED_FOLDER = os.getenv("SHARED_FOLDER", "/app/shared_data")
 os.makedirs(SHARED_FOLDER, exist_ok=True)
 celery = Celery(
     "opencopilot_celery",
-    broker=os.getenv("CELERY_BROKER", "redis://localhost:6379/0"),
-    backend=os.getenv("CELERY_BACKEND", "redis://localhost:6379/1"),
+    broker=os.getenv("CELERY_BROKER", "redis://redis:6379/0"),
+    backend=os.getenv("CELERY_BACKEND", "redis://redis:6379/1"),
 )
