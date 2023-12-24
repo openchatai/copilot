9d91f7247307347e77c91735b82f6edeac5f6b24
ah7255703 <ah7255703@gmail.com>
2023-12-24 12:44:17 +0200

    fix(domain): fix action schema validation for body

--- dashboard/components/domain/action-form/schema.ts
+++ dashboard/components/domain/action-form/schema.ts
@@ -1,22 +1,45 @@
 import { apiMethods } from "@/types/utils";
 import z from "zod";
 
-export const actionSchema = z.object({
-    name: z.string().min(5),
-    description: z.string().min(20),
-    request_type: z.enum(apiMethods),
-    api_endpoint: z.string().url(),
+export enum MIN_CHARS {
+    NAME = 5,
+    DESCRIPTION = 20,
+}
 
+export const actionSchema = z.object({
+    name: z.string().min(MIN_CHARS.NAME),
+    description: z.string().min(MIN_CHARS.DESCRIPTION),
+    request_type: z.enum(apiMethods, {
+        required_error: "Please select a request type",
+    }),
+    api_endpoint: z.string().url('Please enter a valid URL').min(1),
     headers: z.array(z.object({
         key: z.string().min(1),
         value: z.string()
     })).optional(),
-
     parameters: z.array(z.object({
         key: z.string().min(1),
         value: z.string()
     })).optional(),
-    body: z.string().optional(),
+    body: z.string().optional()
+        .refine((value) => {
+            if (typeof value === 'string' && value.trim().length === 0) {
+                return true;
+            }
+            try {
+                if (typeof value !== 'string') {
+                    return false;
+                }
+                JSON.parse(value);
+                return true;
+            } catch (e) {
+                return false;
+            }
+        }, {
+            message: 'Please enter a valid JSON string'
+        })
+
+    , // should be a valid JSON string
 });
 
 export type ActionType = z.infer<typeof actionSchema>;
