26c3dd146b6061b81c94701f506d879d6a0d6712
ah7255703 <ah7255703@gmail.com>
2023-12-24 15:39:49 +0200

    chore(schema): make parameter.value optional if is_magic is true

--- dashboard/components/domain/action-form/schema.ts
+++ dashboard/components/domain/action-form/schema.ts
@@ -5,6 +5,7 @@ export enum MIN_CHARS {
     NAME = 5,
     DESCRIPTION = 20,
 }
+// make parameter.value optional if is_magic is true
 
 export const actionSchema = z.object({
     name: z.string().min(MIN_CHARS.NAME),
@@ -15,12 +16,40 @@ export const actionSchema = z.object({
     api_endpoint: z.string().url('Please enter a valid URL').min(1),
     headers: z.array(z.object({
         key: z.string().min(1),
-        value: z.string()
-    })).optional(),
+        value: z.string(),
+        is_magic: z.boolean().optional().default(false),
+    })).optional().refine((val) => {
+        if (!val) {
+            return true;
+        }
+        return val.every((param) => {
+            if (param.is_magic) {
+                return true;
+            }
+            if (param.value) {
+                return true;
+            }
+            return false;
+        });
+    }),
     parameters: z.array(z.object({
         key: z.string().min(1),
-        value: z.string()
-    })).optional(),
+        value: z.string().optional(),
+        is_magic: z.boolean().optional().default(false),
+    })).optional().refine((val) => {
+        if (!val) {
+            return true;
+        }
+        return val.every((param) => {
+            if (param.is_magic) {
+                return true;
+            }
+            if (param.value) {
+                return true;
+            }
+            return false;
+        });
+    }),
     body: z.string().optional()
         .refine((value) => {
             if (typeof value === 'string' && value.trim().length === 0) {
@@ -38,8 +67,7 @@ export const actionSchema = z.object({
         }, {
             message: 'Please enter a valid JSON string'
         })
-
-    , // should be a valid JSON string
+    ,
 });
 
 export type ActionType = z.infer<typeof actionSchema>;
