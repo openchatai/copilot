5026eaa1a1e08767cf12d25434c107088e3be620
ah7255703 <ah7255703@gmail.com>
2023-12-26 14:26:03 +0200

    Refactor AxiosInstance and createAxiosInstance functions

---
+++ copilot-widget/lib/data/chat.ts
@@ -0,0 +1,25 @@
+import { InitialDataType } from "@lib/types";
+import axios, { AxiosInstance } from "axios";
+
+export function createAxiosInstance(apiUrl?: string, sessionId?: string | null, botToken?: string) {
+    const instance = axios.create({
+        baseURL: apiUrl,
+        headers: {
+            "X-Session-Id": sessionId,
+            "X-Bot-Token": botToken,
+        },
+    });
+
+    instance.interceptors.request.use((config) => {
+        config.data = {
+            ...config.data,
+            session_id: sessionId,
+        };
+        return config;
+    });
+    return instance;
+}
+
+export async function getInitialData(instance: AxiosInstance) {
+    return (await instance.get<InitialDataType>("/chat/init")).data;
+}
