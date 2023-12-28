c321d6fbbb8f4df53a5a0ebdaa7ea85c79737022
ah7255703 <ah7255703@gmail.com>
2023-12-28 17:47:16 +0200

    Remove unused imports and update styles in ActionsList component

--- dashboard/components/domain/new-flows-editor/ActionsList.tsx
+++ dashboard/components/domain/new-flows-editor/ActionsList.tsx
@@ -1,4 +1,3 @@
-import Loader from "@/components/ui/Loader";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { ActionResponseType } from "@/data/actions";
 import { cn } from "@/lib/utils";
@@ -11,10 +10,9 @@ import { methodVariants } from "../MethodRenderer";
 import { format } from "timeago.js";
 import { EmptyBlock } from "../EmptyBlock";
 import { useDraggableInPortal } from "./useDraginPortal";
-import { getStyle } from "./utils";
 
 export function Action({ action }: { action: ActionResponseType }) {
-    return <div className="flex flex-col gap-1 w-full px-4 shrink-0 transition-all py-2 data-[state=open]:!border-l-primary hover:bg-accent !border-transparent border-l-2">
+    return <div className="flex flex-col gap-1 w-full px-4 shrink-0 transition-colors py-2 data-[state=open]:!border-l-primary hover:bg-accent !border-transparent border-l-2">
         <h3 className='text-sm font-semibold'>{action.name}</h3>
         <div className="flex items-center justify-between">
             <span className="text-xs font-medium">
@@ -32,42 +30,40 @@ function DropableAsideAction({ action, index }: { action: ActionResponseType, in
     const draggableInPortal = useDraggableInPortal();
 
     return (
-        <Draggable draggableId={"ACTION|" + action.id} index={index} disableInteractiveElementBlocking>
+        <Draggable draggableId={"ACTION|" + action.id} index={index}>
             {draggableInPortal((provided, snapshot) => {
                 const { isDragging } = snapshot;
-                return <React.Fragment>
-                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={getStyle(provided.draggableProps.style, snapshot)} className="w-full transition-all">
-                        <Popover open={open && !isDragging} onOpenChange={setOpen}>
+                return <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="w-full">
+                    <Popover open={open && !isDragging} onOpenChange={setOpen}>
 
-                            <PopoverTrigger asChild>
-                                <div className={cn('w-full bg-white', isDragging ? "shadow" : " !cursor-pointer")}>
-                                    <Action action={action} />
-                                </div>
-                            </PopoverTrigger>
+                        <PopoverTrigger asChild>
+                            <div className={cn('w-full bg-white h-auto', isDragging ? "shadow" : " !cursor-pointer")}>
+                                <Action action={action} />
+                            </div>
+                        </PopoverTrigger>
 
-                            <PopoverContent side="right" hidden={isDragging} asChild>
-                                <div className="flex flex-col gap-2 text-start overflow-hidden backdrop-blur-sm shadow-none bg-opacity-25">
-                                    <PopoverArrow className="aspect-square w-3 h-2 fill-primary" />
-                                    <div className="">
-                                        <h3 className="text-sm font-semibold">{action.name}</h3>
-                                        <p className="text-xs">{action.description}</p>
-                                    </div>
-                                    <ul className="text-xs space-y-1.5 overflow-x-clip [&>li]:overflow-x-auto [&>li]:py-0.5 [&>li]:no-scrollbar">
-                                        <li>
-                                            <span className="font-medium">Request Type: </span><span>{action.request_type}</span>
-                                        </li>
-                                        <li>
-                                            <span className="font-medium">Request URL: </span><code className="text-xs px-1 py-0.5 bg-black max-w-full whitespace-nowrap select-text text-accent text-left text-white rounded">{action.api_endpoint}</code>
-                                        </li>
-                                        <li>
-                                            <span className="font-medium">Operation Id: </span><code className="text-xs px-1 py-0.5 bg-black select-text text-accent rounded">{action.operation_id}</code>
-                                        </li>
-                                    </ul>
+                        <PopoverContent side="right" hidden={isDragging} asChild>
+                            <div className="flex flex-col gap-2 text-start overflow-hidden backdrop-blur-sm shadow-none bg-opacity-25">
+                                <PopoverArrow className="aspect-square w-3 h-2 fill-primary" />
+                                <div className="">
+                                    <h3 className="text-sm font-semibold">{action.name}</h3>
+                                    <p className="text-xs">{action.description}</p>
                                 </div>
-                            </PopoverContent>
-                        </Popover>
-                    </div>
-                </React.Fragment>
+                                <ul className="text-xs space-y-1.5 overflow-x-clip [&>li]:overflow-x-auto [&>li]:py-0.5 [&>li]:no-scrollbar">
+                                    <li>
+                                        <span className="font-medium">Request Type: </span><span>{action.request_type}</span>
+                                    </li>
+                                    <li>
+                                        <span className="font-medium">Request URL: </span><code className="text-xs px-1 py-0.5 bg-black max-w-full whitespace-nowrap select-text text-accent text-left text-white rounded">{action.api_endpoint}</code>
+                                    </li>
+                                    <li>
+                                        <span className="font-medium">Operation Id: </span><code className="text-xs px-1 py-0.5 bg-black select-text text-accent rounded">{action.operation_id}</code>
+                                    </li>
+                                </ul>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
+                </div>
             })}
         </Draggable>
     )
@@ -84,12 +80,12 @@ export function ActionsList({ disabled }: { disabled?: boolean }) {
             </div>
 
         </EmptyBlock> :
-            <Droppable isDropDisabled={disabled} droppableId={ASIDE_DROPABLE_ID} mode="standard">
+            <Droppable isDropDisabled={disabled} droppableId={ASIDE_DROPABLE_ID}>
                 {
                     (provided) => {
                         return <div ref={provided.innerRef} {...provided.droppableProps} className='w-full shrink-0 divide-y flex overflow-y-hidden min-h-full flex-col items-start justify-start'>
                             {
-                                !actions ? <Loader /> : _.map(actions, (action, index) => <DropableAsideAction key={uniqueId('key')} action={action} index={index} />)
+                                _.map(actions, (action, index) => <DropableAsideAction key={uniqueId('key')} action={action} index={index} />)
                             }
                             {provided.placeholder}
                         </div>
