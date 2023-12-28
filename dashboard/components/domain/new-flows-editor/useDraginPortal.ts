b136e5e79702b2d569b7b461a017a15e1b2789aa
ah7255703 <ah7255703@gmail.com>
2023-12-28 17:46:52 +0200

    Refactor useDraggableInPortal function

--- dashboard/components/domain/new-flows-editor/useDraginPortal.ts
+++ dashboard/components/domain/new-flows-editor/useDraginPortal.ts
@@ -1,23 +1,33 @@
 import { ReactElement, useEffect, useRef } from "react"
 import { DraggableProvided, DraggableStateSnapshot, DraggingStyle } from "react-beautiful-dnd"
 import { createPortal } from "react-dom"
+const PORTAL_ID = 'draggable-portal';
 
+function createElement(id: string): HTMLDivElement {
+    const el = document.getElementById(id);
+    if (el) {
+        return el as HTMLDivElement;
+    }
+    const div = document.createElement('div');
+    div.id = id;
+    return div;
+}
+function setupStyles(element: HTMLDivElement) {
+    element.style.pointerEvents = 'none';
+    element.style.isolation = 'isolate';
+    element.style.position = 'fixed';
+    element.style.inset = '0';
+    element.style.zIndex = '1000';
+    element.style.overflow = 'hidden';
+    return element;
+}
 export const useDraggableInPortal = () => {
-    const element = useRef<HTMLDivElement>(document.createElement('div')).current
-
+    const element = useRef<HTMLDivElement>(createElement(PORTAL_ID)).current
     useEffect(() => {
         if (element) {
-            element.style.pointerEvents = 'none'
-            element.style.isolation = 'isolate'
-            element.style.position = 'absolute'
-            element.style.inset = '0'
-            element.style.zIndex = '9999'
-            document.body.appendChild(element)
+            document.body.appendChild(setupStyles(element))
             return () => {
-                // check if the element was removed by something else
-                if (element.parentElement) {
-                    element.parentElement.removeChild(element)
-                }
+                if (element.parentElement) element.parentElement.removeChild(element)
             }
         }
     }, [element])
