946fbc5fa49304e4a012722b75492c1f2bd37323
ah7255703 <ah7255703@gmail.com>
2024-01-08 01:59:29 +0200

    feat(settings): add variables section

--- dashboard/app/(copilot)/copilot/[copilot_id]/settings/page.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/settings/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useMemo } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -21,6 +21,79 @@ import { toast } from "@/components/ui/use-toast";
 import { CopyButton } from "@/components/headless/CopyButton";
 import { mutate } from "swr";
 import { useAsyncFn } from "react-use";
+import { TableCell } from '@/components/ui/table';
+import { EmptyBlock } from '@/components/domain/EmptyBlock';
+import { Plus } from 'lucide-react';
+import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
+import { Section } from "./Section";
+import { SingleVariableForm } from "./SingleVariableForm";
+import { useVariables } from "./useVariables";
+
+function VariablesSection({ copilot_id }: { copilot_id: string }) {
+  const [vars, createVar, status] = useVariables(copilot_id);
+  const data = useMemo(() => {
+    const _data: { name: string; value: string }[] = [];
+    const __data = vars.data;
+    if (__data) {
+      Object.keys(__data).forEach((key) => {
+        const val = __data[key];
+        _data.push({ name: key, value: val || '' });
+      })
+    }
+    return _data
+  }, [vars])
+
+  return <Section header={<header className="flex items-center justify-between w-full">
+    <h2 className="text-base font-bold">Global Variables</h2>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button size='fit' variant='outline' className="p-1.5">
+          <Plus className='h-4 w-4' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='mx-2'>
+        <SingleVariableForm onSubmit={(data) => { createVar(data.name, data.value) }} footer={<Button loading={status.loading} type='submit' size='xs'>create</Button>} />
+      </PopoverContent>
+    </Popover>
+  </header>}>
+    <div>
+      <table className='w-full table'>
+        <thead>
+          <tr className="[&>th]:p-1">
+            <th className='font-medium text-left'>Name</th>
+            <th className='font-medium text-left'>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {_.isEmpty(['']) ? <tr>
+            <TableCell colSpan={2}>
+              <EmptyBlock />
+            </TableCell>
+          </tr> : data.map((variable, index) => {
+            return <tr className='bg-white [&>td]:p-1' key={index}>
+              <td>
+                <Input defaultValue={variable.name} />
+              </td>
+              <td>
+                <Input defaultValue={variable.value} />
+              </td>
+            </tr>
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={2} className="p-2">
+              <div className="w-full flex gap-2 items-center justify-end">
+                <Button variant='destructiveOutline' size='sm'>Reset</Button>
+                <Button size='sm'>Save</Button>
+              </div>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  </Section>
+}
 
 export default function GeneralSettingsPage() {
   const { token, id: copilotId, name: copilotName } = useCopilot();
@@ -68,9 +141,9 @@ export default function GeneralSettingsPage() {
 
       <div className="flex-1 overflow-auto bg-accent/25 px-4 py-8">
         <div className="container max-w-screen-md space-y-10">
-          <section className="block rounded-lg border border-border bg-white shadow shadow-accent">
-            <div className="space-y-1.5 px-8 py-5">
-              <Label className="text-base font-semibold text-accent-foreground/80">
+          <Section>
+            <div className="space-y-1.5">
+              <Label>
                 Copilot Name
               </Label>
               <div className="flex items-center gap-2">
@@ -84,68 +157,55 @@ export default function GeneralSettingsPage() {
                 />
               </div>
             </div>
-          </section>
-
-          <section className="block ">
-            <h2 className="mb-4 text-base font-bold text-accent-foreground">
-              Metadata
-            </h2>
-            <div className="rounded-lg border border-border bg-white shadow shadow-accent">
-              <div className="space-y-1.5 px-8 py-5">
-                <Label className="text-base font-semibold text-accent-foreground/80">
-                  Token
-                </Label>
-                <div className="flex items-center justify-between gap-2">
-                  <Input className="flex" readOnly defaultValue={token} />
-                  <Button variant="outline" asChild>
-                    <CopyButton text={token}>Copy</CopyButton>
-                  </Button>
-                </div>
-              </div>
+          </Section>
+          <Section title="Metadata">
+            <Label>
+              Token
+            </Label>
+            <div className="flex items-center justify-between gap-2">
+              <Input className="flex" readOnly defaultValue={token} />
+              <Button variant="outline" asChild>
+                <CopyButton text={token}>Copy</CopyButton>
+              </Button>
             </div>
-          </section>
-
-          <section>
-            <h2 className="mb-4 text-base font-bold text-destructive">
-              Danger Zone
-            </h2>
-            <div className="block rounded-lg border border-border bg-white px-8 py-5 shadow shadow-accent">
-              <div className="flex flex-row items-center justify-between">
-                <div>
-                  <h3 className="text-base font-semibold text-accent-foreground">
-                    Delete Copilot
-                  </h3>
-                  <p className="text-sm font-normal">
-                    This action can't be reverted.
-                  </p>
-                </div>
-                <AlertDialog>
-                  <AlertDialogTrigger asChild>
-                    <Button variant="destructive" size="sm">
+          </Section>
+          <VariablesSection copilot_id={copilotId} />
+          <Section title="Danger Zone" intent="danger">
+            <div className="flex flex-row items-center justify-between">
+              <div>
+                <Label className="text-base font-semibold">
+                  Delete Copilot
+                </Label>
+                <p className="text-sm font-normal">
+                  This action can't be reverted.
+                </p>
+              </div>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button variant="destructive" size="sm">
+                    Delete
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    Are you sure you want to delete this assistant?
+                  </AlertDialogHeader>
+                  <AlertDialogDescription>
+                    This action can't be reverted. Please proceed with
+                    caution.
+                  </AlertDialogDescription>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </AlertDialogCancel>
+                    <Button variant="destructive" loading={deleteCopilotstate.loading} onClick={handleDelete}>
                       Delete
                     </Button>
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      Are you sure you want to delete this assistant?
-                    </AlertDialogHeader>
-                    <AlertDialogDescription>
-                      This action can't be reverted. Please proceed with
-                      caution.
-                    </AlertDialogDescription>
-                    <AlertDialogFooter>
-                      <AlertDialogCancel asChild>
-                        <Button variant="outline">Cancel</Button>
-                      </AlertDialogCancel>
-                      <Button variant="destructive" loading={deleteCopilotstate.loading} onClick={handleDelete}>
-                        Delete
-                      </Button>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
-              </div>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </div>
-          </section>
+          </Section>
         </div>
       </div>
     </div>
