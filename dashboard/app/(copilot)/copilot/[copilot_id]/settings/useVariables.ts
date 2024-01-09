e3e7d76f6bfe84ef6c730ce3db08c80575a6b93a
ah7255703 <ah7255703@gmail.com>
2024-01-10 01:06:54 +0200

    refactor: Refactor useVariables function and add async functions for creating and deleting variables

--- dashboard/app/(copilot)/copilot/[copilot_id]/settings/useVariables.ts
+++ dashboard/app/(copilot)/copilot/[copilot_id]/settings/useVariables.ts
@@ -1,15 +1,34 @@
-import { createVariable, getVariablesByBotId } from "@/data/copilot";
+import { createVariable, deleteVariableByKey, getVariablesByBotId, type VariableType } from "@/data/copilot";
 import { useAsyncFn } from "react-use";
 import useSWR from "swr";
 
 export function useVariables(copilot_id: string) {
     const swr = useSWR(copilot_id + '/variables', async () => getVariablesByBotId(copilot_id));
-    const [asyncStatus, createVar] = useAsyncFn(async (name: string, value: string, mutate: boolean = true) => {
-        const { data, status } = await createVariable(copilot_id, name, value);
+
+    const [asyncCreateStatus, createVarAsync] = useAsyncFn(async (vars: VariableType[], mutate: boolean = true) => {
+        const { data, status } = await createVariable(copilot_id, vars);
         if (data.message && status === 200) {
             mutate && swr.mutate()
         }
         return data
     })
-    return [swr, createVar, asyncStatus] as const;
+
+    const [
+        asyncDeleteStatus,
+        deleteVarAsync
+    ] = useAsyncFn(async (key: string) => {
+        const { data, status } = await deleteVariableByKey(copilot_id, key);
+        if (data.message && status === 200) {
+            swr.mutate()
+        }
+        return data
+    })
+
+    return {
+        swr,
+        createVarAsync,
+        asyncCreateStatus,
+        asyncDeleteStatus,
+        deleteVarAsync
+    }
 }
