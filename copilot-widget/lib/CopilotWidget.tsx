b26b9f9f72393843f35280caaf15dbaf24a7ba4a
ah7255703 <ah7255703@gmail.com>
2024-01-12 20:24:26 +0200

    Refactor useTrigger function to handle optional parameters

--- copilot-widget/lib/CopilotWidget.tsx
+++ copilot-widget/lib/CopilotWidget.tsx
@@ -4,14 +4,19 @@ import cn from "./utils/cn";
 import ChatScreenWithSfxs from "./screens/ChatScreen";
 import { IS_SERVER } from "./utils/is_server";
 
-function useTrigger(selector: string, toggle: () => void) {
+function useTrigger(selector?: string, toggle?: () => void) {
+
   const trigger = useRef<HTMLElement | null>(
-    IS_SERVER ? null : document.querySelector(selector)
+    !selector ? null : IS_SERVER ? null : document.querySelector(selector)
   ).current;
+
   useEffect(() => {
+    if (!selector) {
+      return;
+    }
     if (trigger && !IS_SERVER) {
-      trigger.addEventListener("click", toggle);
-      return () => trigger.removeEventListener("click", toggle);
+      trigger.addEventListener("click", () => toggle?.());
+      return () => trigger.removeEventListener("click", () => toggle?.());
     } else {
       console.warn(
         "The trigger element can't be found, make sure it is present in the DOM"
