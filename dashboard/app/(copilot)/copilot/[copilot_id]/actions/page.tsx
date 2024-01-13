0c3893f281f29deb419bfb4d47a6e5cced925236
ah7255703 <ah7255703@gmail.com>
2024-01-14 00:14:47 +0200

    create new action form

--- dashboard/app/(copilot)/copilot/[copilot_id]/actions/page.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/actions/page.tsx
@@ -1,8 +1,11 @@
+'use client';
 import { HeaderShell } from '@/components/domain/HeaderShell'
 import { RouteIcon } from 'lucide-react';
 import { ActionsDataGrid } from './ActionsDataGrid';
 import { Suspense } from 'react';
 import { Stack } from '@/components/ui/Stack';
+import { Button } from '@/components/ui/button';
+import { AddActionDrawer, useActionFormState } from '@/components/domain/new-flows-editor/addActionDrawer';
 
 type Props = {
     params: {
@@ -11,18 +14,26 @@ type Props = {
 }
 
 export default function ActionsPage({ params }: Props) {
+    const [
+        ,
+        setActionFormState
+    ] = useActionFormState();
     return (
-        <Stack 
-        ic='center'
-        direction='column'
-        fluid
-        className='size-full'>
+        <Stack
+            ic='center'
+            direction='column'
+            fluid
+            className='size-full'>
             <HeaderShell className='justify-between'>
                 <div className='flex items-center gap-2'>
                     <RouteIcon size={24} />
-                    <h1 className='text-lg font-semibold'>Flows</h1>
+                    <h1 className='text-lg font-semibold'>Actions</h1>
+                </div>
+                <div>
+                    <Button onClick={() => setActionFormState(true)} variant='default'>Create</Button>
                 </div>
             </HeaderShell>
+            <AddActionDrawer />
             <div className='flex-1 w-full p-4 overflow-auto'>
                 <Suspense fallback={<div>Loading...</div>}>
                     <ActionsDataGrid copilot_id={params.copilot_id} />
