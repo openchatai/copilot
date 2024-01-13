821603aa36504f71477c54a43cde654c380ff3de
ah7255703 <ah7255703@gmail.com>
2024-01-14 00:13:09 +0200

    separate `Action` Component and implement Edit Action

--- dashboard/app/(copilot)/copilot/[copilot_id]/actions/ActionsDataGrid.tsx
+++ dashboard/app/(copilot)/copilot/[copilot_id]/actions/ActionsDataGrid.tsx
@@ -1,44 +1,17 @@
 'use client';
 import { Input } from "@/components/ui/input";
-import { ActionResponseType } from "@/data/actions";
 import { useListActions } from "@/hooks/useActions";
-import { cn } from "@/lib/utils";
-import { ColumnDef, ColumnFiltersState, GroupingState, Row, RowSelectionState, SortingState, VisibilityState, getCoreRowModel, getFilteredRowModel, getGroupedRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
-import { useCallback, useRef, useState } from "react";
-import { methodVariants } from "@/components/domain/MethodRenderer";
+import { ColumnDef, ColumnFiltersState, RowSelectionState, SortingState, getCoreRowModel, getFilteredRowModel, getGroupedRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
+import { useCallback, useState } from "react";
 import { Stack } from "@/components/ui/Stack";
 import { format } from "timeago.js";
-import { Settings2, Trash2 } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import _ from "lodash";
+import { EmptyBlock } from "@/components/domain/EmptyBlock";
+import { DataTablePagination } from "@/components/ui/TablePagination";
+import { Action } from "./Action";
+import { ActionWithModifiedParametersResponse } from "@/data/actions";
 
-function Action({ getValue, getIsSelected, toggleSelected, order }: Row<ActionResponseType> & { order: number }) {
-    const containerRef = useRef<HTMLDivElement>(null);
-    const isSelected = getIsSelected();
-    return <Stack
-        ref={containerRef}
-        ic="start"
-        gap={5}
-        direction="column"
-        className={cn('bg-secondary p-3.5 rounded-lg col-span-full lg:col-span-6 xl:col-span-4 border-2', isSelected ? 'border-primary/50 bg-secondary shadow-sm' : 'border-transparent')}>
-        <Stack ic="start" gap={10} id="some" direction="row">
-            <div className='text-sm font-semibold flex-1'>{getValue("name")}</div>
-            {/* @ts-ignore */}
-            <div className={methodVariants({ size: "tiny", method: String(getValue("request_type")).toUpperCase() })}>{getValue("request_type")}</div>
-        </Stack>
-        <div className='text-xs text-gray-500'>{getValue("description")}</div>
-        <Stack className="mt-auto gap-1" js="end">
-            <Button variant='outline' size='fit' onClick={() => toggleSelected()}>
-                <Settings2 size={15} />
-            </Button>
-            <Button variant='destructiveOutline' size='fit'>
-                <Trash2 size={15} />
-            </Button>
-        </Stack>
-    </Stack>
-}
-
-const columns: ColumnDef<ActionResponseType>[] = [
+const columns: ColumnDef<ActionWithModifiedParametersResponse>[] = [
     {
         accessorKey: "id",
         id: "id"
@@ -67,21 +40,20 @@ export function ActionsDataGrid({ copilot_id }: { copilot_id: string }) {
     const { data: actions } = useListActions(copilot_id);
     const [sorting, setSorting] = useState<SortingState>([]);
     const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
     const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
-    const [grouping, setGrouping] = useState<GroupingState>([]);
+
     const grid = useReactTable({
         data: actions || [],
         columns,
-        onColumnVisibilityChange: setColumnVisibility,
+        pageCount: 1,
         onRowSelectionChange: setRowSelection,
         onSortingChange: setSorting,
         onColumnFiltersChange: setColumnFilters,
-        onGroupingChange: setGrouping,
         getCoreRowModel: getCoreRowModel(),
         getSortedRowModel: getSortedRowModel(),
         getFilteredRowModel: getFilteredRowModel(),
         getGroupedRowModel: getGroupedRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
         enableFilters: true,
         enableColumnFilters: true,
         enableSorting: true,
@@ -89,45 +61,39 @@ export function ActionsDataGrid({ copilot_id }: { copilot_id: string }) {
         state: {
             sorting,
             columnFilters,
-            columnVisibility,
             rowSelection,
-            grouping
         }
     })
+
     const filterByName = useCallback((name: string) => {
         grid.getColumn("name")?.setFilterValue(name)
     }, [
         grid
     ])
-    const groupBy = useCallback((method: "request_type" | "none") => {
-        grid.setGrouping(prev => [...prev, method])
-    }, [grid])
-    console.log(grid.getGroupedRowModel())
+    console.log(
+        grid.getSelectedRowModel().rows
+    )
     return (
-        <div>
+        <div className="container">
             <Stack fluid direction="row" js="between" className="!gap-5">
                 <Input placeholder="Search"
                     className="flex-1"
                     onChange={(e) => filterByName(e.target.value)}
                 />
-                <Select name="group_by" defaultValue="none" onValueChange={(v) => groupBy(v as any)}>
-                    <SelectTrigger className="shrink min-w-fit w-20">
-                        <SelectValue placeholder="Group By" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="none">None</SelectItem>
-                        <SelectItem value="request_type">Method</SelectItem>
-                    </SelectContent>
-                </Select>
             </Stack>
             <div className="grid gap-4 grid-cols-12 mt-5">
-                {grid.getGroupedRowModel().rows.map((item, index) => {
-                    return (
-                        <Action key={index} {...item} order={index} />
-                    )
-                })}
+                {_.isEmpty(grid.getRowModel().rows) ?
+                    <EmptyBlock>
+                        <h1 className="text-lg font-semibold">No actions found</h1>
+                        <p className="text-sm text-gray-500">Create a new action to get started</p>
+                    </EmptyBlock>
+                    : grid.getRowModel().rows.map((item, index) => (
+                        <Action key={index} {...item} />
+                    ))}
+            </div>
+            <div hidden={grid.getPageCount() === 1} className="mt-4">
+                <DataTablePagination table={grid} />
             </div>
         </div>
     )
-
 }
