b4dc0b748e47c1aa8db0343bcab3a437abd29665
gharbat <arbioun@gmail.com>
2024-01-21 23:02:47 -0800

    Chate votes migrations

---
+++ llm-server/shared/models/opencopilot_db/chat_votes.py
@@ -0,0 +1,22 @@
+import datetime
+
+from sqlalchemy import Column, String, DateTime, Boolean, Integer, JSON
+
+from .database_setup import engine
+from .get_declarative_base import Base
+
+class ChatVote(Base):
+    __tablename__ = "chat_votes"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    chatbot_id = Column(String(36), nullable=True)
+    message_id = Column(String(255), nullable=True)
+    is_upvote = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(
+        DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow
+    )
+
+
+
+Base.metadata.create_all(engine)
