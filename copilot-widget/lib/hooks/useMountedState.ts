2708bba92d0ed748eb47be746120ce4e148543dd
ah7255703 <ah7255703@gmail.com>
2024-01-25 00:50:19 +0200

    add `useUp&Down` vote impl, and useAsyncFn impl.

---
+++ copilot-widget/lib/hooks/useMountedState.ts
@@ -0,0 +1,16 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export default function useMountedState(): () => boolean {
+  const mountedRef = useRef<boolean>(false);
+  const get = useCallback(() => mountedRef.current, []);
+
+  useEffect(() => {
+    mountedRef.current = true;
+
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  return get;
+}
