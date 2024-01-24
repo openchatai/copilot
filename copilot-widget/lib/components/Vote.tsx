3ad7d578f0d8432730d3e9ecc0ee2eaa7c996c52
ah7255703 <ah7255703@gmail.com>
2024-01-25 00:50:35 +0200

    Refactor Vote component to use async upvote and downvote

--- copilot-widget/lib/components/Vote.tsx
+++ copilot-widget/lib/components/Vote.tsx
@@ -1,3 +1,4 @@
+import { useDownvote, useUpvote } from '@lib/hooks/useVote';
 import cn from '@lib/utils/cn';
 import {
     ThumbsUp,
@@ -6,25 +7,23 @@ import {
 
 const SIZE = 26;
 
-export function Vote({
-    onUpvote,
-    onDownvote,
-    isUpvoted = false,
-    isDownvoted = true,
-}: {
-    onUpvote?: () => void,
-    onDownvote?: () => void,
-    isUpvoted: boolean,
-    isDownvoted: boolean,
-}) {
+export function Vote({ messageId }: { messageId: number }) {
+    const [asyncUpvoteState, asyncUpvote] = useUpvote(String(messageId));
+    const [asyncDownvoteState, asyncDownvote] = useDownvote(String(messageId));
+    const isUpvoted = !!asyncUpvoteState.value?.data.message;
+    const isDownvoted = !!asyncDownvoteState.value?.data.message;
+    const userVoted = isUpvoted || isDownvoted;
     return (
         <div className='opencopilot-flex opencopilot-items-center opencopilot-justify-end opencopilot-w-full opencopilot-gap-px [&>button]:opencopilot-p-1'>
-            <button data-active={isUpvoted} className={cn('opencopilot-transition-all opencopilot-rounded-lg', isUpvoted ? '*:opencopilot-fill-primary' : 'active:*:opencopilot-scale-105')} onClick={() => onUpvote?.()}>
-                <ThumbsUp size={SIZE} className='opencopilot-transition-all opencopilot-stroke-primary' />
-            </button>
-            <button data-active={isDownvoted} className={cn('opencopilot-transition-all opencopilot-rounded-lg', isDownvoted ? '*:opencopilot-fill-rose-500' : 'active:*:opencopilot-scale-105')} onClick={() => onDownvote?.()}>
-                <ThumbsDown size={SIZE} className='opencopilot-transition-all opencopilot-stroke-rose-500' />
-            </button>
+            {
+                userVoted ? <span className='opencopilot-text-xs text-blur-out opencopilot-text-emerald-500'>thank you</span> : <><button onClick={asyncUpvote} className={cn('opencopilot-transition-all opencopilot-rounded-lg', isUpvoted ? '*:opencopilot-fill-emerald-500' : 'active:*:opencopilot-scale-105')}>
+                    <ThumbsUp size={SIZE} className='opencopilot-transition-all opencopilot-stroke-emerald-500' />
+                </button>
+                    <button onClick={asyncDownvote} className={cn('opencopilot-transition-all opencopilot-rounded-lg', isDownvoted ? '*:opencopilot-fill-rose-500' : 'active:*:opencopilot-scale-105')}>
+                        <ThumbsDown size={SIZE} className='opencopilot-transition-all opencopilot-stroke-rose-500' />
+                    </button></>
+            }
+
         </div>
     )
 }
