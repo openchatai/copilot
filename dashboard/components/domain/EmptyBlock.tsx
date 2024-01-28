5967484f0d917b6a507753619bc86dbf4dd7f333
ah7255703 <ah7255703@gmail.com>
2024-01-29 00:51:24 +0200

    Refactor EmptyBlock component to allow custom empty component and image URL

--- dashboard/components/domain/EmptyBlock.tsx
+++ dashboard/components/domain/EmptyBlock.tsx
@@ -5,18 +5,20 @@ type Props = {
   Imagesize?: number;
   children?: React.ReactNode;
   className?: string;
+  imageUrl?: string;
+  emptyCompnent?: React.ReactNode;
 };
 
-export function EmptyBlock({ Imagesize = 60, children, className }: Props) {
+export function EmptyBlock({ Imagesize = 60, children, className, emptyCompnent, imageUrl }: Props) {
   return (
     <div className={cn("flex-center flex-col gap-2 py-4 animate-in fade-in", className)}>
-      <Image
-        src="/random_icons_2.svg"
+      {emptyCompnent ?? <Image
+        src={imageUrl ?? "/random_icons_2.svg"}
         width={Imagesize}
         height={Imagesize}
         alt="Empty Icon"
         className="aspect-square"
-      />
+      />}
       {children}
     </div>
   );
