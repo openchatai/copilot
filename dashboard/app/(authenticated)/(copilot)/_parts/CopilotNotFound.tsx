bab40dead6a359120ff19dc147f37be13d8ac5e5
ah7255703 <ah7255703@gmail.com>
2024-01-29 00:51:31 +0200

    Update CopilotNotFound component styling

--- dashboard/app/(authenticated)/(copilot)/_parts/CopilotNotFound.tsx
+++ dashboard/app/(authenticated)/(copilot)/_parts/CopilotNotFound.tsx
@@ -4,9 +4,11 @@ import React from "react";
 export function CopilotNotFound() {
   return (
     <div className="flex-center h-full w-full">
-      <EmptyBlock Imagesize={100}>
-        <h2 className="text-center text-xl font-semibold">Copilot not found</h2>
-      </EmptyBlock>
+      <div className="bg-white py-5 px-10 border rounded-lg">
+        <EmptyBlock Imagesize={100}>
+          <h2 className="text-center text-2xl font-bold">Copilot not found</h2>
+        </EmptyBlock>
+      </div>
     </div>
   );
 }
