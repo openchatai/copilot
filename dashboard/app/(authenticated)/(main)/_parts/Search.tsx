9541e8da26cf99e714135143727e93e6a010486a
Ahmad Falta <76843311+ah7255703@users.noreply.github.com>
2024-01-29 03:47:02 +0200

    Update Search.tsx

--- dashboard/app/(authenticated)/(main)/_parts/Search.tsx
+++ dashboard/app/(authenticated)/(main)/_parts/Search.tsx
@@ -19,30 +19,28 @@ export function Search() {
   const [filter, setFilter] = useAtom(filterAtom);
 
   return (
-    <div className="flex items-center justify-between gap-5 py-5">
-      <div className="flex-1 max-w-lg relative">
-        <Label htmlFor="search-copilots" className="absolute left-2 top-1/2 -translate-y-1/2">
-          <SearchIcon className="h-5 w-5 opacity-50" />
-        </Label>
-        <DebounceInput
-          // @ts-ignore
-          element={Input}
-          debounceTimeout={300}
-          value={filter.query}
-          type="text"
-          onChange={(event) => {
-            setFilter((prev) => {
-              return {
-                ...prev,
-                query: event.target.value.trim(),
-              };
-            });
-          }}
-          id="search-copilots"
-          className="font-medium ps-8 placeholder:text-xs focus-visible:!ring-transparent"
-          placeholder="Search Copilots..."
-        />
-      </div>
+    <div className="w-full lg:max-w-lg relative py-5">
+      <Label htmlFor="search-copilots" className="absolute left-2 top-1/2 -translate-y-1/2">
+        <SearchIcon className="h-5 w-5 opacity-50" />
+      </Label>
+      <DebounceInput
+        // @ts-ignore
+        element={Input}
+        debounceTimeout={300}
+        value={filter.query}
+        type="text"
+        onChange={(event) => {
+          setFilter((prev) => {
+            return {
+              ...prev,
+              query: event.target.value.trim(),
+            };
+          });
+        }}
+        id="search-copilots"
+        className="font-medium ps-8 placeholder:text-xs focus-visible:!ring-transparent"
+        placeholder="Search Copilots..."
+      />
     </div>
   );
 }
