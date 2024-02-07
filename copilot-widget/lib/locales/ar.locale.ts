10cf25969a0adce0bcbf1eb46f289d498b1939a1
ah7255703 <ah7255703@gmail.com>
2024-02-07 02:52:33 +0200

    Add Arabic translations for new phrases

--- copilot-widget/lib/locales/ar.locale.ts
+++ copilot-widget/lib/locales/ar.locale.ts
@@ -4,6 +4,12 @@ const arLocale: Locale = {
   ok: "حسنا",
   agree: "موافق",
   cancel: "إلغاء",
+  "yes-exit": "نعم، اخرج",
+  "yes-reset": "نعم، إعادة تعيين",
+  "no-cancel": "لا، إلغاء",
+  "are-you-sure": "هل أنت متأكد؟",
+  recording: "تسجيل",
+  "thank-you": "شكرا",
 };
 
 export default arLocale;
