e85a71cb026d39311c8cec43a6a26c8e0310994b
ah7255703 <ah7255703@gmail.com>
2024-02-07 02:26:52 +0200

    Refactor locale helper and add index file

--- copilot-widget/lib/locales/helper.ts
+++ copilot-widget/lib/locales/helper.ts
@@ -1,16 +1,14 @@
-import enLocale from './en.locale';
-import arLocale from './ar.locale';
+import enLocale from "./en.locale";
+import arLocale from "./ar.locale";
 // Import more locales as needed
 
 const locales = {
-    en: enLocale,
-    ar: arLocale,
-    // Add more locales as needed
+  en: enLocale,
+  ar: arLocale,
+  // Add more locales as needed
 };
 
-function getLocaleString(key: string, lang: keyof typeof locales): string {
-    const locale = locales[lang];
-    return locale ? locale[key] || '' : '';
+export function getStr(key: string, lang: keyof typeof locales): string {
+  const locale = locales[lang];
+  return locale ? locale[key] || "" : "";
 }
-
-export default getLocaleString;
