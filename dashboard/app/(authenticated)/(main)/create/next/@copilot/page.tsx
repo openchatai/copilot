f49c0456193a728255d8061ed16dc0fabd23d6bf
ah7255703 <ah7255703@gmail.com>
2024-02-10 04:47:12 +0200

    chore: Remove unnecessary files and code

--- dashboard/app/(authenticated)/(main)/create/next/@copilot/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CopilotSigment() {
-  return <div>CopilotSigment</div>;
-}
--- dashboard/app/(authenticated)/(main)/create/next/@someothertype/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SomeOtherType() {
-  return <div>SomeOtherType</div>;
-}
