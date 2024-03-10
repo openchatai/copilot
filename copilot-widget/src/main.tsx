55012a0ac168b863cb6e21c68fefeb37877e69c1
ah7255703 <ah7255703@gmail.com>
2024-03-10 02:20:25 +0200

    Refactor main.tsx: Remove unused component and update widget initialization

--- copilot-widget/src/main.tsx
+++ copilot-widget/src/main.tsx
@@ -14,49 +14,6 @@ declare global {
   }
 }
 
-// This is an Example Component
-function C_get_current_business_managers_api_lists_businessmanagers_get({
-  data,
-}: {
-  data: {
-    Email: "dfilippi@burrburton.org";
-    FirstName: "DENNIS";
-    LastName: "FILIPPI";
-    OrgID: "PA002";
-    OrgName: "BURR AND BURTON ACADEMY";
-    Phone: "8023621775";
-    ServerId: "1";
-  }[];
-}) {
-  return (
-    <div className="max-w-full w-full overflow-auto">
-      <div>List of Business Managers</div>
-      <table className="text-xs whitespace-nowrap">
-        <thead>
-          <tr className="*:p-0.5 text-start">
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Organization</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, i) => (
-            <tr key={i} className="*:p-0.5 text-start">
-              <td>{row.FirstName}</td>
-              <td>{row.LastName}</td>
-              <td>{row.Email}</td>
-              <td>{row.Phone}</td>
-              <td>{row.OrgName}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
 /**
  * @param rootId The id of the root element for more control, you don't need to use this unless you want more control over the widget
  * @description Initialize the widget
@@ -72,13 +29,6 @@ function initAiCoPilot({
     <Root
       options={{
         ...options,
-        components: [
-          {
-            key: "get_current_business_managers_api_lists_businessmanagers_get",
-            component:
-              C_get_current_business_managers_api_lists_businessmanagers_get,
-          },
-        ],
       }}
       containerProps={containerProps}
     >
